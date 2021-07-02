43354073398d064dc440b80f2c076b0683a6e839
Антон Каданцев <kadantsev.anton@yandex.ru>
2021-07-02 13:09:28 +0300

    Закончил первый урок. Введение в ООП. Создание первого класса. Вызванный класс это объект. Экземпляр - это поля. Объект - это методы. Так разделяются вызванные класс в ОЗУ

--- C#/20210701_Console/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/20210701_Console/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,12 +2,25 @@
 
 namespace ConsoleApp1
 {
+    class MyClass
+    {
+        public int field;
+        public void Method() 
+        {
+            Console.WriteLine(field);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            MyClass a = new MyClass();
+            a.field = 1;
+            a.Method();
+
+            MyClass b = new MyClass();
+            b.field = 2;
+            b.Method();
         }
     }
 }
