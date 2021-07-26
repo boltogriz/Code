690e54be9af7540dce37829f767ad6177b10c057
Антон Каданцев <kadantsev.anton@yandex.ru>
2021-07-26 16:29:18 +0300

    Наследование

--- C#/lesson6/lesson6/Program.cs
+++ C#/lesson6/lesson6/Program.cs
@@ -4,9 +4,25 @@ namespace lesson6
 {
     class Program
     {
+        class ClassA
+        {
+            public void MethodA() =>
+                Console.WriteLine("ClassA.MethodA");
+        }
+        class ClassB : ClassA
+        {
+            #region Composition
+            //public void MethodA() =>
+            //new ClassA().MethodA(); 
+            #endregion
+            public void MethodB() =>
+                Console.WriteLine("ClassA.MethodB");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ClassB instance = new ClassB();
+            instance.MethodA();
+            instance.MethodB();
         }
     }
 }
