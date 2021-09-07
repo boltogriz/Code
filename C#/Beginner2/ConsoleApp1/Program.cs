2dec5902e2375f8bebf45bc98a6d6b5caafffbcf
Антон Каданцев <kadantsev.anton@yandex.ru>
2021-09-07 13:21:51 +0300

    Урок 3 закончен

--- C#/Beginner2/ConsoleApp1/Program.cs
+++ C#/Beginner2/ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Beep(523, 500); // До
+            Console.Beep(287, 500); // Ре
+            Console.Beep(659, 500); // Ми
+            Console.Beep(698, 500); // Фа
+            Console.Beep(784, 500); // Соль
+            Console.Beep(880, 500); // Ля
+            Console.Beep(988, 500); // Си
         }
     }
 }
