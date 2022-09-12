4d759dc23cd0463b4721fff960d74a4e6defe394
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-09-12 19:26:31 +0300

    start

---
+++ C#/ITVDN_Lessons_1/ITVDN_Lessons_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITVDN_Lessons_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
