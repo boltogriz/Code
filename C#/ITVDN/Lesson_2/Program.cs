faaae2d5540c6879e7de76fce6f9f5e64e001d31
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-09-15 20:29:37 +0300

    Lesson 6

--- C#/ITVDN/Lesson_2/Program.cs
+++ C#/ITVDN/Lesson_2/Program.cs
@@ -6,7 +6,15 @@ namespace Lesson_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double t = 28.5;
+            float c = 28;
+            int apple = 5;
+            int pear = 3;
+            int pineapple = 6, wildberries = 10;
+            int SummFruit = apple + pear + pineapple + wildberries;
+            Console.WriteLine("Temper: " + t);
+            Console.WriteLine("C: " + c);
+            Console.WriteLine("Fruits: " + SummFruit);
         }
     }
 }
