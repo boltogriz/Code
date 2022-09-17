db1d90f2e5f9ae69eac79a8a26dd01c29bade4f5
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-09-17 11:41:30 +0300

    Система счисления

--- C#/ITVDN/Lesson_3/Program.cs
+++ C#/ITVDN/Lesson_3/Program.cs
@@ -6,7 +6,27 @@ namespace Lesson_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Nober;
+            string Comand = "start";
+            SystemCount sys_count = new SystemCount();
+            sys_count.ChengSysCount(); //вводит ситему счиление ввода и вывода, создает свойства которые потом используются
+            while(Comand != "exit")
+            {
+                Console.WriteLine("Введите " + sys_count.SysCountEnter + " чило:");
+                Comand = Console.ReadLine();
+                if (Comand == "reset")
+                {
+                    sys_count.ChengSysCount();
+                    continue;
+                }
+                if (Comand == "exit") break;
+                try 
+                {
+                    Nober = Convert.ToInt32(Comand, Convert.ToInt32(sys_count.SysCountEnter));
+                    Console.WriteLine(Convert.ToString(Nober, Convert.ToInt32(sys_count.SysCountWithdow)) + " - система счисления " + sys_count.SysCountWithdow + "\n");
+                }
+                catch { Console.WriteLine("Ошибка ввода\n"); }
+            }
         }
     }
 }
---
+++ C#/ITVDN/Lesson_3/SystemCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    class SystemCount
+    {
+        public string SysCountEnter { get; set; }
+        public string SysCountWithdow { get; set; }
+        public void ChengSysCount()
+        {
+            Console.WriteLine("Введите систему счисления ввода: ");
+            SysCountEnter = Console.ReadLine();
+            Console.WriteLine("Введите систему счисления вывода: ");
+            SysCountWithdow = Console.ReadLine();
+            Console.WriteLine("\n");
+        }
+
+
+
+    }
+}
