8a9822ac3f555f65bac31d4f5ea7904bb7f87cd8
Антон Каданцев <kadantsev.anton@yandex.ru>
2022-09-23 16:13:07 +0300

    lesson 14

---
+++ C#/ITVDN_2022/Lesson_10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Chart = "Hollow World!";
+            char letter = 'A';
+            char letter2 = Convert.ToChar("A");
+
+            Console.WriteLine(Chart);
+            Console.WriteLine(letter);
+            Console.WriteLine(letter2);
+
+            //Delay
+            Console.ReadLine();
+        }
+    }
+}
