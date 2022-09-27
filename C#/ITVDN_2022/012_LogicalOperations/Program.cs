7fe2275a150dc0b66d0101ee6eea024e4e88c559
Антон Каданцев <kadantsev.anton@yandex.ru>
2022-09-27 10:52:34 +0300

    Lesson 16

---
+++ C#/ITVDN_2022/012_LogicalOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_LogicalOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue;
+            double x = double.NaN, y = double.NaN;
+
+            isTrue = x == y;
+            Console.WriteLine($"{x} == {y}: {isTrue}");
+
+            isTrue = x != y;
+            Console.WriteLine($"{x} != {y}: {isTrue}");
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
