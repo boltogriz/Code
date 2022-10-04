aa296b69abf341be65f792bb75dd51a5f0a55ea3
Антон Каданцев <kadantsev.anton@yandex.ru>
2022-10-04 13:15:24 +0300

    22

---
+++ C#/ITVDN_2022/017_Checked/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _017_Checked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint minutesQuarter;
+            {
+                Console.Write("Введите количетство минут разговоров за январь: ");
+                uint minutesJanuary = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Введите количетство минут разговоров за февраль: ");
+                uint minutesFebruary = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Введите количетство минут разговоров за март: ");
+                uint minutesMarch = Convert.ToUInt32(Console.ReadLine());
+
+                checked
+                {
+                    minutesQuarter = minutesFebruary + minutesJanuary + minutesMarch;
+                }
+            }
+            decimal totalCost; // руб.
+            {
+                decimal price = 2; // руб.
+                totalCost = minutesQuarter * price;
+            }
+
+            decimal tax;
+            {
+                decimal taxRate = 20; // %
+                tax = totalCost / 100 * taxRate;
+            }
+
+            Console.WriteLine($"Всего               : {minutesQuarter} минут");
+            Console.WriteLine($"Стоимость всех минут: {totalCost} рублей") ;
+            Console.WriteLine($"Заплатить налог     : {tax} рублей");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ITVDN_2022/019_Unchecked/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _019_Unchecked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                int a = int.MaxValue;
+                int b = a + 1;
+                Console.WriteLine(b);
+
+                unchecked
+                {
+                    int c = int.MaxValue;
+                    int d = c + 1;
+                    Console.WriteLine(d);
+                }
+                int e = int.MinValue;
+                int f = e - 1;
+                Console.WriteLine(f);
+            }
+            int x = int.MinValue;
+            int y = checked(x + 1);
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
