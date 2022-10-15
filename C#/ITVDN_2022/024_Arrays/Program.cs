e74c695c67156e1b09fbf208275658ec931ac214
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-10-15 13:16:41 +0300

    31

--- C#/ITVDN_2022/024_Arrays/Program.cs
+++ C#/ITVDN_2022/024_Arrays/Program.cs
@@ -10,15 +10,30 @@ namespace _024_Arrays
     {
         static void Main(string[] args)
         {
-            byte[] ageList = { 23, 30, 35 };
-            //byte age = ageList[0];
-            ageList[2] = 55;
-            int index = 0;
-            while (index < 3)
             {
-                byte ages = ageList[index];
-                Console.WriteLine(ages);
-                index++;
+                byte[] ageList = { 23, 30, 35 };
+                //byte age = ageList[0];
+                ageList[2] = 55;
+                int index = 0;
+                while (index < 3)
+                {
+                    byte ages = ageList[index];
+                    Console.WriteLine(ages);
+                    index++;
+                }
+            }
+            {
+                Console.Write("введите размер массива: ");
+                int length = Convert.ToInt32(Console.ReadLine());
+                byte[] array = new byte[length];
+                int index = 0;
+                while (index < array.Length)
+                {
+                    array[index] = (byte)index;
+                    Console.WriteLine($"{array[index]}");
+                    index++;
+                }
+                 
             }
             Console.ReadKey();
         }
