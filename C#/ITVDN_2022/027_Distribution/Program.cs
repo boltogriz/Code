f11893dde7f671b6a67ea5551c2cefdd30c33add
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-10-18 18:56:32 +0300

    35

--- C#/ITVDN_2022/027_Distribution/Program.cs
+++ C#/ITVDN_2022/027_Distribution/Program.cs
@@ -13,14 +13,16 @@ namespace _027_Distribution
             string[] months = { "январь", "февраль", "март" };
             string[] tiles = { "Агат", "Оникс", "Малахит" };
             string[] distributors = { "Мир керамики", "Дом плитки" };
-            decimal[,,] array = new decimal[ distributors.Length, tiles.Length, months.Length];
-            for (int z = 0; z < array.GetLength(0); z++)
-                for (int y = 0; y < array.GetLength(1); y++)
-                    for (int x = 0; x < array.GetLength(2); x++)
-                    {
-                        Console.Write($"{distributors[z]} продала плитки {tiles[y]} за {months[x]}: ");
-                        array[z, y, x] = Convert.ToDecimal(Console.ReadLine());
-                    }
+            string[] countries = { "Италия", "Испания" };
+            decimal[,,,] array = new decimal[countries.Length, distributors.Length, tiles.Length, months.Length];
+            for (int w = 0; w < array.GetLength(0); w++)
+                for (int z = 0; z < array.GetLength(1); z++)
+                    for (int y = 0; y < array.GetLength(2); y++)
+                        for (int x = 0; x < array.GetLength(3); x++)
+                        {
+                            Console.Write($"{distributors[z]} продала плитки {tiles[y]} за {months[x]} в {countries[w]}: ");
+                            array[w, z, y, x] = Convert.ToDecimal(Console.ReadLine());
+                        }
             while (true)
             {
                 string distributorName;
@@ -52,8 +54,9 @@ namespace _027_Distribution
                     }
                 } while (tailIndex == -1);
                 decimal numberOfTiles = 0;
-                for (int x = 0; x < array.GetLength(2); x++)
-                    numberOfTiles += array[distributorIdex, tailIndex, x];
+                for (int w = 0; w < array.GetLength(0); w++)
+                    for (int x = 0; x < array.GetLength(3); x++)
+                        numberOfTiles += array[w, distributorIdex, tailIndex, x];
                 Console.WriteLine($"{distributorName} продал {numberOfTiles} шт. плитки {tileName} за квартал.");
             }
             //Console.ReadKey();
