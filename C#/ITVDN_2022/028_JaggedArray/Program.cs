f3092d99b8b0bcb0db6085f6e20d86d1f62fd1aa
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-10-19 18:08:40 +0300

    36

---
+++ C#/ITVDN_2022/028_JaggedArray/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _028_JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                byte[] array0 = new byte[] { 1, 2, 3 };
+                byte[] array1 = new byte[] { 4, 5, 6 };
+                byte[] array2 = new byte[] { 7, 8, 9 };
+                byte[][] array = new byte[3][] { array0, array1, array2 };
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        Console.Write($"{array[i][j]} ");
+                    }
+                }
+            }
+            {
+                Console.WriteLine();
+                byte[] array0 = new byte[] { 1, 2, 3 };
+                byte[] array1 = new byte[] { 4, 5, 6 };
+                byte[] array2 = new byte[] { 7, 8, 9 };
+                byte[][] array = new byte[3][];
+                array[0] = array0;
+                array[1] = array1;
+                array[2] = array2;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        Console.Write($"{array[i][j]} ");
+                    }
+                }
+            }
+            {
+                Console.WriteLine();
+                byte[] array0 = new byte[] { 1, 2, 3 };
+                byte[] array1 = new byte[] { 4, 5, 6 };
+                byte[] array2 = new byte[] { 7, 8, 9 };
+                byte[][] array = new byte[3][]
+                    {
+                     new byte[] { 1, 2, 3 },
+                     new byte[] { 4, 5, 6 },
+                     new byte[] { 7, 8, 9 }
+                    };
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        Console.Write($"{array[i][j]} ");
+                    }
+                }
+            }
+
+            {
+                Console.WriteLine();
+                byte[,] array0 = new byte[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+                byte[,] array1 = new byte[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+                byte[,] array2 = new byte[,] { { 1, 2, 3 }, { 4, 5, 9 } };
+                byte[][,] array = new byte[3][,] { array0, array1, array2 };
+                for (int i = 0; i < array.Length; i++)
+                    for (int j = 0; j < array[i].GetLength(0); j++)
+                        for (int k = 0; k < array[i].GetLength(1); k++)
+                            Console.Write($"{array[i][j, k]} ");
+            }
+            {
+                Console.WriteLine("\n====\n");
+                //byte[,] array0 = new byte[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+                //byte[,] array1 = new byte[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+                //byte[,] array2 = new byte[,] { { 1, 2, 3 }, { 4, 5, 9 } };
+                //byte[][,] array = new byte[3][,] { array0, array1, array2 };
+                byte[][,] array = new byte[3][,];
+                array[0] = new byte[,]{ { 1, 2, 3 }, { 4, 5, 6 } };
+                array[1] = new byte[,]{ { 1, 2, 3 }, { 4, 5, 6 } };
+                array[2] = new byte[,]{ { 1, 2, 3 }, { 4, 5, 6 } };
+                for (int i = 0; i < array.Length; i++)
+                    for (int j = 0; j < array[i].GetLength(0); j++)
+                        for (int k = 0; k < array[i].GetLength(1); k++)
+                            Console.Write($"{array[i][j, k]} ");
+            }
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+
+                byte[,][,] array = new byte[3, 2][,] ;
+                array[0, 0] = new byte[,] { { 1, 2, 3}, {1, 2, 3 } };
+                array[0, 1] = new byte[,] { { 1, 2}, {1, 2 } };
+                array[1, 0] = new byte[,] { { 1, 2, 3}, {1, 2, 3 } };
+                array[1, 1] = new byte[,] { { 1, 2, 3, 4}, {1, 2, 3, 4 } };
+                array[2, 0] = new byte[,] { { 1, 2, 3}, {1, 2, 3 } };
+                array[2, 1] = new byte[,] { { 1, 2, 3}, {1, 2, 3 } };
+                array[2, 1] = new byte[,] { { 1}, {1} };
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < array[i, j].GetLength(0); k++)
+                        {
+                            for (int l = 0; l < array[i, j].GetLength(1); l++)
+                            { 
+                                Console.Write($"{array[i, j][k, l]} "); 
+                            }
+                        Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                byte[,] array01 = new byte[,] { { 1,2,3 },{ 1,2,3 } };
+                byte[,][,] array = new byte[3, 2][,];
+                array[0, 0] = new byte[,] { { 1, 2, 3 }, { 1, 2, 3 } };
+                array[0, 1] = new byte[,] { { 1, 2 }, { 1, 2 } };
+                array[1, 0] = new byte[,] { { 1, 2, 3 }, { 1, 2, 3 } };
+                array[1, 1] = new byte[,] { { 1, 2, 3, 4 }, { 1, 2, 3, 4 } };
+                array[2, 0] = new byte[,] { { 1, 2, 3 }, { 1, 2, 3 } };
+                array[2, 1] = new byte[,] { { 1, 2, 3 }, { 1, 2, 3 } };
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < array[i, j].GetLength(0); k++)
+                        {
+                            for (int l = 0; l < array[i, j].GetLength(1); l++)
+                            {
+                                Console.Write($"{array[i, j][k, l]} ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            {
+                byte[,] array0 = new byte[2, 5], array1 = new byte[4, 5], array2 = new byte[3, 10],
+                                 array3 = new byte[2, 5], array4 = new byte[4, 5], array5 = new byte[4, 1],
+                                 array6 = new byte[2, 5], array7 = new byte[4, 5], array8 = new byte[3, 10],
+                                 array9 = new byte[2, 5], array10 = new byte[4, 5], array11 = new byte[3, 10], array12 = new byte[2,20];
+                byte[,][,] ja1 = new byte[3,2][,] { { array0, array1}, { array2, array3}, {array5, array6} }; 
+                byte[,][,] ja2 = new byte[3,2][,] { { array7, array8}, { array9, array10}, {array11, array12 } };
+                byte[][,][,] jaggedArray = new byte[2][,][,] { ja1, ja2 };
+                for (int i = 0; i < jaggedArray.Length; i++)
+                {
+                    for (int j = 0; j < jaggedArray[i].GetLength(0); j++)
+                    {
+                        for (int k = 0; k < jaggedArray[i].GetLength(1); k++)
+                        {
+                            for (int l = 0; l < jaggedArray[i][j, k].GetLength(0); l++)
+                            {
+                                for (int m = 0; m < jaggedArray[i][j, k].GetLength(1); m++)
+                                {
+                                    Console.Write($"{jaggedArray[i][j, k][l, m]}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ITVDN_2022/029_SalesReport/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _029_SalesReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Запросить и принять план продаж
+            decimal plan;
+            {
+                Console.Write("Введите план продаж (в руб.): ");
+                plan = Convert.ToDecimal(Console.ReadLine());
+            }
+            //2. Запросить и принять список фамилий продажников
+            string[] surnameArray;
+            {
+                Console.WriteLine("Введите фамили сотрудников через запятую: ");
+                string surnames = Console.ReadLine(); ;
+                surnameArray = surnames.Split(',');
+                for (int i = 0; i < surnameArray.Length; i++)
+                    surnameArray[i] = surnameArray[i].Trim();
+            }
+            //3. Запросить и принять список сумм продаж для каждого продажкника
+            decimal[][] jaggedArray;
+            {
+                jaggedArray = new decimal[surnameArray.Length][];
+                for (int i = 0; i < jaggedArray.Length; i++)
+                {
+                    Console.WriteLine($"Введите через запятую суммы продаж которые совершил {surnameArray[i]}");
+                    string sums = Console.ReadLine();
+                    string[] sumsArray = sums.Split(',');
+                    jaggedArray[i] = new decimal[sumsArray.Length];
+                    for (int j = 0; j < sumsArray.Length; j++)
+                    {
+                        string sum = sumsArray[j].Trim();
+                        jaggedArray[i][j] = Convert.ToDecimal(sum);
+                    }
+                }
+            }
+            //4. Формирование массива общих сумм продаж по каждому продажнику
+            decimal[] totalsumsArray;
+            {
+                totalsumsArray = new decimal[jaggedArray.Length];
+                for (int i = 0; i < totalsumsArray.Length; i++)
+                {
+                    decimal totalSum = 0;
+                    for (int j = 0; j < jaggedArray[i].Length; j++)
+                    {
+                        totalSum += jaggedArray[i][j];
+                    }
+                    totalsumsArray[i] = totalSum;
+                }
+            }
+            //5. Формирование массива общих сумм продаж по каждому продажнику
+            {
+                for (int i = 0; i < totalsumsArray.Length; i++)
+                {
+                    Console.Write($"{surnameArray[i]} продал товара на сумму {totalsumsArray[i]} руб. ");
+                    decimal persent;
+                    if (totalsumsArray[i] < plan)
+                    {
+                        persent = (plan - totalsumsArray[i]) / (plan / 100);
+                        Console.WriteLine($"План недовыполнен на {persent} %");
+                    }
+                    else if (totalsumsArray[i] == plan)
+                    {
+                        Console.WriteLine($"План выполнен на 100%");
+                    }
+                    else if (totalsumsArray[i] > plan)
+                    {
+                        persent = (totalsumsArray[i] - plan) / (plan / 100);
+                        Console.WriteLine($"План перевыполнен на {persent} %");
+                    }
+                }
+            }
+            //6. Формирование отчета о минимальной и максимальной продаже для каждого сейла
+            {
+                for (int i = 0; i < jaggedArray.Length; i++)
+                {
+                    Array.Sort(jaggedArray[i]);
+                    int lastIndex = jaggedArray[i].Length - 1;
+                    Console.WriteLine($"{surnameArray[i]}: Мин. продажа = {jaggedArray[i][0]}, Макс. продажа {jaggedArray[i][lastIndex]}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
