4c4b67733b43acecc97ec64e77678feabccd845a
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-11-04 13:13:12 +0300

    ке

--- C#/TestNuGet/TestNuGet/Program.cs
+++ C#/TestNuGet/TestNuGet/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Limilabs.Client.SMTP;
+using Limilabs.Mail.Headers;
+using Limilabs.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
 
 namespace TestNuGet
 {
@@ -10,6 +14,27 @@ namespace TestNuGet
     {
         static void Main(string[] args)
         {
+            MailBuilder builder = new MailBuilder();
+            builder.From.Add(new MailBox("a.kadantsev@mtsr.krasnodar.ru"));
+            builder.To.Add(new MailBox("kadantsev.anton@gmail.com"));
+            builder.Subject = "Subject";
+            builder.Html = @"Html with an image: <img src=""cid:lena"" />";
+
+            //var visual = builder.AddVisual(@"c:\lena.jpeg");
+            //visual.ContentId = "lena";
+            var attachment = builder.AddAttachment(@"c:\tmp.doc");
+            attachment.SetFileName("document.doc", guessContentType: true);
+
+            IMail email = builder.Create();
+
+            using (Smtp smtp = new Smtp())
+            {
+                smtp.ConnectSSL("m.krasnodar.ru");  // or ConnectSSL for SSL
+                smtp.UseBestLogin("MTSR/a.kadantsev", "Vbhnhelvfq1");
+
+                smtp.SendMessage(email);
+                smtp.Close();
+            }
         }
     }
 }
