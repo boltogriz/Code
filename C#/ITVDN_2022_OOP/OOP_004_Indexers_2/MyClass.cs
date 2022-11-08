66b4ba3e2a69bedaffa24d6a717abe1dc524cdbf
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-11-08 12:25:36 +0300

    lesson 6 end

---
+++ C#/ITVDN_2022_OOP/OOP_004_Indexers_2/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_004_Indexers_2
+{
+    internal class MyClass
+    {
+        int[,] array = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 0 } };
+        public int this[int index1, int index2]
+        {
+            get => array[index1, index2];
+            set => array[index1, index2] = value;
+        }
+        //public int GetLength(int dimension) => array.GetLength(dimension);
+        public int GetLength(int dimension) { return array.GetLength(dimension); }
+    }
+}
---
+++ C#/ITVDN_2022_OOP/OOP_004_Indexers_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_004_Indexers_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance[0, 0] = 777;
+            for (int i = 0; i < instance.GetLength(0); i++)
+            {
+                for (int j = 0; j <instance.GetLength(1); j++)
+                {
+                Console.WriteLine(instance[i, j]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
