1a2fd535b78c4908946c59f989683cce2c1965a9
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-11-11 15:35:00 +0300

    lesson 7 50 min

---
+++ C#/ITVDN_2022_OOP/OOP_004_Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_004_Inheritance
+{
+    class ClassA
+    {
+        public void MethodA() => Console.WriteLine("ClassA.MethodA");
+    }
+    class ClassB : ClassA
+    {
+        #region Composition
+        //public void MethodA() => new ClassA().MethodA(); 
+        #endregion
+        public void MethodB() => Console.WriteLine("ClassB.MethodB");
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassB instance = new ClassB();
+            instance.MethodA();
+            instance.MethodB();
+            new ClassA().MethodA();
+            Console.ReadKey();
+        }
+    }
+}
