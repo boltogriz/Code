648795addaf126a6e136e2d761edf7b817bd80b0
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-12-04 20:26:49 +0300

    lesson 7

---
+++ C#/ITVDN_Essential/007_ITVDN_Structur/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _007_ITVDN_Structur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Struct instance;
+           // instance.field = 1;
+           // Console.WriteLine(instance.field);
+            Struct instance2 = new Struct(10);
+            instance2.Show();
+        }
+    }
+} 
---
+++ C#/ITVDN_Essential/007_ITVDN_Structur/Struct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_ITVDN_Structur
+{
+    struct Struct
+    {
+        public int field;
+        public int field2;
+        static int second;
+        public int Field
+        {
+            get { return field; }
+            set { field = value; }
+        }
+        public void Show()
+        {
+            Console.WriteLine(field);
+        }
+        int item;
+        public Struct(int item)
+
+        {
+            this.item = item;
+            field = 4;
+            field2 = 4;
+        }
+    }
+}
