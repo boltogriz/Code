d79e8ab7ab8a0095937ad54a66da88491ff67ee1
Антон Каданцев <kadantsev.anton@yandex.ru>
2022-12-08 16:54:37 +0300

    lesson 10

---
+++ C#/ITVDN_Essential/010_ITVDN_UneveralShablon/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_ITVDN_UneveralShablon
+{
+    internal class MyClass<T>
+    {
+        public T Field { get; set; }
+        public void Method()
+        {
+            Console.WriteLine(Field.GetType());
+        }
+    }
+}
---
+++ C#/ITVDN_Essential/010_ITVDN_UneveralShablon/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _010_ITVDN_UneveralShablon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> instance = new MyClass<int>();
+            instance.Method();
+        }
+    }
+}
