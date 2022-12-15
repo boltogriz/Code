f39129c46c92ab8d845e92d1cb66bad2bfaeafc1
Антон Каданцев <kadantsev.anton@yandex.ru>
2022-12-15 11:17:50 +0300

    11

---
+++ C#/ITVDN_Essential/011_ITVDN_Constrane/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _011_ITVDN_Constrane
+{
+    class MyClass<T> where T : struct
+    { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyClass<int> instance = new MyClass<int>();
+        }
+    }
+}
