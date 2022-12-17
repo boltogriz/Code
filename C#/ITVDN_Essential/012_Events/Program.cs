c3a9a8d45fff54ee28927d3b78d8a3da4fd6039e
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-12-17 13:41:28 +0300

    12

---
+++ C#/ITVDN_Essential/012_Events/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _012_Events
+{
+    public delegate void EventDelegate();
+    public class MyClass
+    {
+        public event EventDelegate myEvent = null;
+        public void invokeEvent()
+        {
+            myEvent.Invoke();
+        }
+    }
+    internal class Program
+    {
+        static private void Handler1()
+        {
+            Console.WriteLine("Обработчик события 1");
+        }
+        static private void Handler2()
+        {
+            Console.WriteLine("Обработчик события 2");
+        }
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance.myEvent += new EventDelegate(Handler1);
+            instance.myEvent += Handler2;
+            instance.invokeEvent();
+        }
+    }
+}
