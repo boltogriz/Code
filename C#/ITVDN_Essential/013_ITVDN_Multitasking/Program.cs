71597fbff9a66bbc009cbde9d2f50a14f14cdc79
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-12-27 21:05:55 +0300

    13

---
+++ C#/ITVDN_Essential/013_ITVDN_Multitasking/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _013_ITVDN_Multitasking
+{
+    internal class Program
+    {
+        static void WriteSecond()
+        {
+            while (true)
+            {
+                Console.WriteLine(new String(' ', 10) + "Secondary");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart writeSecond = new ThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecond);
+            thread.Start();
+            while (true)
+            {
+                Console.WriteLine("Primary");
+            }
+        }
+    }
+}
