9929e3abbef75837993091f3429f9e81b2daa443
Антон Каданцев <kadantsev.anton@yandex.ru>
2022-12-28 15:59:40 +0300

    15

---
+++ C#/ITVDN_Essential/014_ITVDN_Collection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _014_ITVDN_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
