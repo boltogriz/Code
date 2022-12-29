c5268804be44e5e60697117359fcafa457fa0f8b
Anton Kadantsev <kadantsev.anton@yandex.ru>
2022-12-29 21:18:56 +0300

    15

--- C#/ITVDN_Essential/014_ITVDN_Collection/Program.cs
+++ C#/ITVDN_Essential/014_ITVDN_Collection/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Exception ex = new Exception("Мое Исключение");
+            try
+            {
+                throw ex;
+                int a = 0, n = 2;
+                a = a / (2 - n);
+                Console.WriteLine("a={0}", 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
