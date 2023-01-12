67e8354430125c258af8b660554ce0eb0494437c
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-01-12 21:32:56 +0300

    MyCalcTest 31 min

---
+++ C#/UnitTest/MyCalcLibs.Tests/MyCalcTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyCalcLibs.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Sum_10and20_30returned()
+        {
+            // arrange
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            // act
+            MyCalcLibs c = new MyCalcLibs();
+            int actual = c.Sum(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
