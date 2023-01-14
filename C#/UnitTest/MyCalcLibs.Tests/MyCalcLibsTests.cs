12a529d5268c415187894fa042bd7d52dc2446cc
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-01-14 21:24:41 +0300

    1 00

---
+++ C#/UnitTest/MyCalcLibs.Tests/MyCalcLibsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalcLibs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalcLibs.Tests
+{
+    [TestClass()]
+    public class MyCalcLibsTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
