2bd7e2cfdc9df304b687d6ba08b5818de9ef9b40
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-01-23 16:18:40 +0300

    50 mines

--- C#/Essential/Essential/Program.cs
+++ C#/Essential/Essential/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Essential
 {
+    class Butterfly
+    {
+        public string name;
+        public string Name { get => name; set => name = value; }
+        public void Fly()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Fly!");
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Butterfly mahaon = new Butterfly();
+            mahaon.Name = "Admiral";
+            mahaon.Fly();
+            Console.ReadLine();
         }
     }
 }
