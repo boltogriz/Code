1bc48008c6740f44a5043a43647ca6c50e0fab7b
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-01-29 14:30:06 +0300

    Lesson 2, 8 minutes

---
+++ C#/Essential/02_Partial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Partial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialClass partialClass1 = new PartialClass();
+            partialClass1.Method1();
+            partialClass1.Method2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Essential/WinFormsApp1/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            string xyz = "x";
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
