c4ed0369b6f5eebeb1cc11aecb089d8c44773140
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-01-30 20:31:35 +0300

    22 minutes

--- C#/Essential/02_Partial/Program.cs
+++ C#/Essential/02_Partial/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace _02_Partial
 {
+    class MyClass
+    {
+        MyClass my = null;
+        private void Method()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void CallMethod()
+        {
+            my = new MyClass();
+            my.Method();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -13,6 +26,9 @@ namespace _02_Partial
             PartialClass partialClass1 = new PartialClass();
             partialClass1.Method1();
             partialClass1.Method2();
+            Console.WriteLine(new String('-', 20));
+            MyClass my = new MyClass();
+            my.CallMethod();
             Console.ReadKey();
         }
     }
