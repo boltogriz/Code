8be0175339891fa0e48c137efc720850e40bf99b
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-08 23:16:14 +0300

    Lesson 4 end

--- C#/Essential/04_Abstraction_Interface/IMyInterface.cs
+++ C#/Essential/04_Abstraction_Interface/IMyInterface.cs
@@ -11,6 +11,22 @@ namespace _04_Abstraction_Interface
         void Method();
         void MethodPrivet();
     }
+    abstract class AbstractClass3 : IMyInterface
+    {
+        public abstract void Method();
+        public abstract void MethodPrivet();
+    }
+    class MyClassForAbst : AbstractClass3
+    {
+        public override void Method()
+        {
+            Console.WriteLine("AbstractClass3 Method");
+        }
+        public override void MethodPrivet()
+        {
+            Console.WriteLine("AbstractClass3 MethodPrivet");
+        }
+    }
     class MyClass : IMyInterface
     {
         public void Method()
--- C#/Essential/04_Abstraction_Interface/Program.cs
+++ C#/Essential/04_Abstraction_Interface/Program.cs
@@ -8,6 +8,30 @@ namespace _04_Abstraction_Interface
 {
     internal class Program
     {
+        interface IAnimal
+        {
+            void Action();
+        }
+        class Dog : IAnimal
+        {
+            public void Action()
+            {
+                Console.WriteLine("Gaf");
+            }
+            public void Guard()
+            {
+                Console.WriteLine("Guard");
+            }
+        }
+        class Cat : IAnimal
+        {
+            public void Action()
+            {
+                Console.WriteLine("Meu");
+            }
+            
+        }
+
         static void Main(string[] args)
         {
             AbstractClass instance = new ConcreteClass();
@@ -26,6 +50,21 @@ namespace _04_Abstraction_Interface
             Console.WriteLine(new string('-', 20));
             IMyInterface myInt = my as IMyInterface;
             myInt.MethodPrivet();
+            MyClass myClass = new MyClass();
+            myClass.Method();
+            Console.WriteLine(new string('-', 20));
+            MyClassForAbst myClassForAbst = new MyClassForAbst();
+            myClassForAbst.Method();
+            myClassForAbst.MethodPrivet();
+            Console.WriteLine(new string('-', 20));
+            IAnimal cat = new Cat();
+            Dog dog = new Dog();
+            IAnimal cat2 = dog as IAnimal;
+            dog.Guard();
+            Dog dog2 = cat2 as Dog;
+            dog2.Guard();
+            cat.Action();
+            dog.Action();
             Console.ReadKey();
         }
     }
