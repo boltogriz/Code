e4e739808ac2adbf94da4e2ad4aebe35fcb69611
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-02-09 16:36:03 +0300

    Lesson 5, minutes 6

---
+++ C#/Essential/05_Massive_Indecsator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Massive_Indecsator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] arr = new byte [3];
+        }
+    }
+}
