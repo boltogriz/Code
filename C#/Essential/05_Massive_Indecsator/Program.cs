fb18dd56c1601d4701cb14212ff8759cfca464bd
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-09 20:28:16 +0300

    Lesson 5, minutes 21

--- C#/Essential/05_Massive_Indecsator/Program.cs
+++ C#/Essential/05_Massive_Indecsator/Program.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 
 namespace _05_Massive_Indecsator
 {
+    public interface IAnimal
+    {
+        void Voice();
+    }
+    public class Dog : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Voice");
+        }
+        public void Jump()
+        {
+            Console.WriteLine("Jump");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             byte[] arr = new byte [3];
+            Dog[] dogs = { new Dog(), new Dog(), new Dog() };
+            for(int i = 0; i < dogs.Length; i++)
+            {
+                dogs[i].Voice();
+                dogs[i].Jump();
+            }
+            Console.WriteLine(new String('-', 20));
+            IAnimal[] animal = dogs;
+            for(int i = 0; i < animal.Length; i++)
+            {
+                animal[i].Voice();
+                //animal[i].Jump();
+            }
+            Console.WriteLine(new String('-', 20));
+            dogs = (Dog[])animal;
+            for(int i = 0; i < dogs.Length; i++)
+            {
+                dogs[i].Voice();
+                dogs[i].Jump();
+            }
+            Console.WriteLine(new String('-', 20));
+            Console.ReadKey();
         }
     }
 }
