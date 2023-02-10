0844dc36b99c7d3566442107642797069c9a78c0
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-10 17:52:47 +0300

    Lesson 5, minutes 31

--- C#/Essential/05_Massive_Indecsator/Program.cs
+++ C#/Essential/05_Massive_Indecsator/Program.cs
@@ -12,6 +12,12 @@ namespace _05_Massive_Indecsator
     }
     public class Dog : IAnimal
     {
+        string[] array = new string[3];
+        public string this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
         public void Voice()
         {
             Console.WriteLine("Voice");
@@ -23,8 +29,16 @@ namespace _05_Massive_Indecsator
     }
     internal class Program
     {
+        static void ShowArray(params int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+        }
         static void Main(string[] args)
         {
+
             byte[] arr = new byte [3];
             Dog[] dogs = { new Dog(), new Dog(), new Dog() };
             for(int i = 0; i < dogs.Length; i++)
@@ -35,10 +49,10 @@ namespace _05_Massive_Indecsator
             Console.WriteLine(new String('-', 20));
             IAnimal[] animal = dogs;
             for(int i = 0; i < animal.Length; i++)
-            {
-                animal[i].Voice();
-                //animal[i].Jump();
-            }
+            {                                     
+                animal[i].Voice();                
+                //animal[i].Jump();               
+            }                                     
             Console.WriteLine(new String('-', 20));
             dogs = (Dog[])animal;
             for(int i = 0; i < dogs.Length; i++)
@@ -47,6 +61,14 @@ namespace _05_Massive_Indecsator
                 dogs[i].Jump();
             }
             Console.WriteLine(new String('-', 20));
+            ShowArray(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+            Console.WriteLine();
+            Console.WriteLine(new String('-', 20));
+            Dog dog = new Dog();
+            dog[0] = "0";
+            dog[1] = "1";
+            dog[2] = "2";
+            Console.WriteLine(dog[0]);
             Console.ReadKey();
         }
     }
