3fe4d7390173b7102f58ec731aa94df3be342978
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-12 18:21:04 +0300

    Lesson 6, minutes 13

---
+++ C#/Essential/06_Static/NotStaticClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Static
+{
+    internal class NotStaticClass
+    {
+        public static int field;
+        public void Method()
+        {
+            Console.WriteLine("Static field {0}", field);
+        }
+        public NotStaticClass()
+        {
+            field = 0;
+        }
+        public static void MethodStatic()
+        {
+            Console.WriteLine("Static Method()");
+        }
+    }
+}
---
+++ C#/Essential/06_Static/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotStaticClass instance = new NotStaticClass();
+            NotStaticClass instance1 = new NotStaticClass();
+            instance.Method();
+            instance1.Method();
+            NotStaticClass.field = 1;
+            instance.Method();
+            instance1.Method();
+            NotStaticClass.MethodStatic();
+            Console.ReadKey();
+        }
+    }
+}
