d2f3af26227db2edf3570cfa717397ef780cdc98
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-02-15 16:07:02 +0300

    Lesson 7, minutes 10

---
+++ C#/Essential/07_Struct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Struct
+{
+    struct Struct
+    {
+        public int field;
+        public static int second;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Struct s;
+            s.field = 1;
+            Struct.second = 2;
+        }
+    }
+}
