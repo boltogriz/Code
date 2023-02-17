07c1050f5730f2b7aaabaf83d843569d4d2ee4af
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-02-17 15:32:47 +0300

    Lesson 7 end

---
+++ C#/Essential/07_Struct/MyStruct.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Struct
+{
+    struct MyStruct_8kb
+    {
+        long a0;
+        long a1;   
+
+        long a2;
+        long a3;
+        long a4;
+        long a5;
+            
+            
+            
+        long a6;
+        long a7;
+        long a8;
+            
+        long a9;
+        long a10;
+            
+            
+        long a11;
+        long a12;
+        long a13;
+        long a14;
+        long a15;
+        long a16;
+        long a17;
+        long a18;
+        long a19;
+        long a20;
+        long a21;
+        long a22;
+        long a23;
+        long a24;
+        long a25;
+        long a26;
+        long a27;
+        long a28;
+        long a29;
+        long a30;
+        long a31;
+        long a32;   
+        long a33;
+        long a34;
+        long a35;
+        long a36;
+        long a37;
+        long a38;
+        long a39;
+        long a40;
+        long a41;
+        long a42;
+        long a43;
+        long a44;
+        long a45;
+        long a46;
+        long a47;
+        long a48;
+        long a49;
+        long a50;
+        long a51;
+        long a52;
+        long a53;
+        long a54;
+        long a55;
+        long a56;
+        long a57;
+        long a58;
+        long a59;
+        long a60;
+
+    }
+    struct Mystruct_16kb
+    {
+        public long a0;
+        public long a1;
+
+        long a2;
+        long a3;
+        MyStruct_8kb a4;
+        long a5;
+
+
+
+        MyStruct_8kb a6;
+        long a7;
+        MyStruct_8kb a8;
+
+        long a9;
+        MyStruct_8kb a10;
+
+
+        MyStruct_8kb a11;
+        MyStruct_8kb a12;
+        long a13;
+        long a14;
+        long a15;
+        long a16;
+        long a17;
+        MyStruct_8kb a18;
+        long a19;
+        MyStruct_8kb a20;
+        long a21;
+        long a22;
+        long a23;
+        long a24;
+        MyStruct_8kb a25;
+        MyStruct_8kb a26;
+        MyStruct_8kb a27;
+        long a28;
+        MyStruct_8kb a29;
+        MyStruct_8kb a30;
+        MyStruct_8kb a31;
+        long a32;
+        MyStruct_8kb a33;
+        long a34;
+        MyStruct_8kb a35;
+        MyStruct_8kb a36;
+        MyStruct_8kb a37;
+        MyStruct_8kb a38;
+
+
+
+    }
+    struct MyStruct_1mb
+    {
+        public Mystruct_16kb a0;
+        Mystruct_16kb a1;
+        Mystruct_16kb a2;
+        Mystruct_16kb a3;
+        Mystruct_16kb a4;
+            Mystruct_16kb a5;
+        Mystruct_16kb a6;
+        Mystruct_16kb a7;
+        Mystruct_16kb a8;
+        Mystruct_16kb a9;
+        Mystruct_16kb a10;
+            Mystruct_16kb a11;
+        Mystruct_16kb a12;
+        Mystruct_16kb a13;
+        Mystruct_16kb a14;
+        Mystruct_16kb a15;
+        Mystruct_16kb a16;
+            Mystruct_16kb a17;
+        Mystruct_16kb a18;
+        Mystruct_16kb a19;
+        Mystruct_16kb a20;
+        Mystruct_16kb a21;
+        Mystruct_16kb a22;
+        Mystruct_16kb a23;
+        Mystruct_16kb a24;
+        Mystruct_16kb a25;
+        Mystruct_16kb a26;
+        Mystruct_16kb a27;
+        Mystruct_16kb a28;
+        Mystruct_16kb a29;
+        Mystruct_16kb a30;
+        Mystruct_16kb a31;
+        Mystruct_16kb a32;
+        Mystruct_16kb a33;
+            Mystruct_16kb a34;
+        Mystruct_16kb a35;
+        Mystruct_16kb a36;
+        Mystruct_16kb a37;
+        Mystruct_16kb a38;
+        Mystruct_16kb a39;
+        Mystruct_16kb a40;
+        Mystruct_16kb a41;
+        Mystruct_16kb a42;
+        Mystruct_16kb a43;
+        Mystruct_16kb a44;
+        Mystruct_16kb a45;
+        Mystruct_16kb a46;
+            Mystruct_16kb a47;
+        Mystruct_16kb a48;
+        Mystruct_16kb a49;
+        Mystruct_16kb a50;
+        Mystruct_16kb a51;
+        Mystruct_16kb a52;
+        Mystruct_16kb a53;
+        Mystruct_16kb a54;
+            Mystruct_16kb a55;
+        Mystruct_16kb a56;
+        Mystruct_16kb a57;
+        Mystruct_16kb a58;
+        Mystruct_16kb a59;
+        Mystruct_16kb a60;
+        Mystruct_16kb a61;
+        Mystruct_16kb a62;
+        Mystruct_16kb a63;
+        Mystruct_16kb a64;
+
+
+
+    }
+}
--- C#/Essential/07_Struct/Program.cs
+++ C#/Essential/07_Struct/Program.cs
@@ -11,13 +11,23 @@ namespace _07_Struct
         public int field;
         public static int second;
     }
+    class MyClass
+    {
+        public MyStruct_1mb st = new MyStruct_1mb();
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Struct s = new Struct();
-            s.field = 1;
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Struct s = new Struct() { field = 2};
+            //s.field = 1;
+            Console.WriteLine(s.field);
             Struct.second = 2;
+            MyClass my = new MyClass();
+            my.st.a0.a1 = 1;
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Console.ReadKey();
         }
     }
 }
