a3f2f7135d5e581a9dce18825eca90e71f156a00
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-18 21:30:14 +0300

    Lesson 8, minutes 40

--- C#/Essential/08_Boxing/Program.cs
+++ C#/Essential/08_Boxing/Program.cs
@@ -8,11 +8,37 @@ namespace _08_Boxing
 {
     internal class Program
     {
+        public interface IAnimal
+        {
+            void Method();
+        }
+        public struct Dog : IAnimal
+        {
+            public void Method()
+            {
+                Console.WriteLine("Voice");
+            }
+        }
+        struct MyStruct
+        {
+            public void Method()
+            {
+                Console.WriteLine("Method");
+            }
+        }
         static void Main(string[] args)
         {
             short a = 25;
             object o = a;
             short b = (short)o;
+            MyStruct my = new MyStruct();
+            ValueType boxed = my;
+            MyStruct unBoxed = (MyStruct)boxed;
+            Dog[] dogs = { new Dog(), new Dog(), new Dog() };
+            IAnimal animal = (IAnimal)dogs[0];
+            object @object = dogs;
+            Console.ReadKey();
+            
 
         }
     }
