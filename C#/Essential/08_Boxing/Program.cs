e072f6beb9017eb93ab2174e5f4ef1aa070daa2d
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-18 11:56:39 +0300

    Lesson 8, minutes 2

---
+++ C#/Essential/08_Boxing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Boxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 25;
+            object o = a;
+            short b = (short)o;
+
+        }
+    }
+}
