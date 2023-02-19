7f3d9182dec7dc358657e08535cec2f0bea29d1a
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-19 13:56:54 +0300

    Lesson 8, minutes 1:16

--- C#/Essential/08_Boxing/Program.cs
+++ C#/Essential/08_Boxing/Program.cs
@@ -26,6 +26,36 @@ namespace _08_Boxing
                 Console.WriteLine("Method");
             }
         }
+        enum EnumType : byte
+        {
+            Zero = 0,
+            One = 1,
+            Two = 2,
+            Three = 3,
+            x,
+            y,
+            z = 24,
+            i = 12,
+            u,
+            v
+        }
+        static void MethodEnum(EnumType e)
+        {
+            switch (e)
+            {
+                case EnumType.Zero:
+                    Console.WriteLine("1. {0} = {1}", EnumType.Zero, (byte)EnumType.Zero);
+                    break;
+                case EnumType.One:
+                    Console.WriteLine("2. {0} = {1}", EnumType.One, (byte)EnumType.One);
+                    break;
+                case EnumType.Three:
+                    Console.WriteLine("3. {0} = {1}", EnumType.Three, (byte)EnumType.Three);
+                    break;
+
+                default: break;
+            }
+        }
         static void Main(string[] args)
         {
             short a = 25;
@@ -37,6 +67,31 @@ namespace _08_Boxing
             Dog[] dogs = { new Dog(), new Dog(), new Dog() };
             IAnimal animal = (IAnimal)dogs[0];
             object @object = dogs;
+
+            Console.WriteLine(EnumType.One);
+            Console.WriteLine((byte)EnumType.One);
+
+            EnumType digit = EnumType.Zero;
+            Console.WriteLine(digit);
+            Console.WriteLine((byte)digit);
+            Console.WriteLine(new string('-', 20));
+            for(EnumType i = EnumType.Zero; i <= EnumType.v; i++)
+            {
+                Console.WriteLine(i +" = " + (byte)i);
+                
+            }    
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("{0} = {1}", EnumType.z, (byte)EnumType.z);
+            Console.WriteLine(new string('-', 20));
+            EnumType enumTyp = EnumType.Three;
+            MethodEnum(enumTyp);
+            Console.WriteLine(new string('-', 20));
+            EnumType digit2 = EnumType.Three;
+            Type @enum = digit2.GetType();
+            Console.WriteLine("1. Перечисление {0} состоит из элементов типа: ", @enum.Name);
+            Console.WriteLine(Enum.GetUnderlyingType(@enum));
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(EnumType)));
+
             Console.ReadKey();
             
 
