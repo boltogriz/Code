63df72f56e48f51f47633521363b5d91c0ec7071
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-21 20:06:28 +0300

    Lesson 9, minutes 40

--- C#/Essential/09_Delegat/Program.cs
+++ C#/Essential/09_Delegat/Program.cs
@@ -22,7 +22,15 @@ namespace _09_Delegat
                 Console.WriteLine("MyClass2.Method()");
             }
         }
+        class MyClass3
+        {
+            public string Method(string name)
+            {
+                return name + " MyClass3";
+            }
+        }
         public delegate void MyDelegate();
+        public delegate string MyDelegate2(string name);
         static void Main(string[] args)
         {
             MyDelegate myDelegate = new MyDelegate(MyClass.Method);
@@ -32,8 +40,14 @@ namespace _09_Delegat
             MyDelegate myDelegate2 = new MyDelegate(myClass2.Method);
             myDelegate2.Invoke();
             myDelegate2();
-            // задержка
 
+            MyClass3 myClass3 = new MyClass3();
+            MyDelegate2 myDelegate3 = new MyDelegate2(myClass3.Method);
+            string d3 = myDelegate3.Invoke("myDelegate30000");
+            string d4 = myDelegate3("myDelegate40000");
+            Console.WriteLine(d4);
+            
+            // задержка
             Console.ReadKey();
         }
     }
