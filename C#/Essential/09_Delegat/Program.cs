f74d2b33087e9c8152937a2cf7d26680ec88b4ef
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-02-22 11:38:11 +0300

    Lesson 9, minutes 51

--- C#/Essential/09_Delegat/Program.cs
+++ C#/Essential/09_Delegat/Program.cs
@@ -8,6 +8,23 @@ namespace _09_Delegat
 {
     internal class Program
     {
+        public delegate void MyDelegateProgram();
+        public static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+        public static void Method3()
+        {
+            Console.WriteLine("Method3");
+        }
+        public static void Method4()
+        {
+            Console.WriteLine("Method4");
+        }
         public class MyClass 
         {
             public static void Method()
@@ -46,7 +63,31 @@ namespace _09_Delegat
             string d3 = myDelegate3.Invoke("myDelegate30000");
             string d4 = myDelegate3("myDelegate40000");
             Console.WriteLine(d4);
-            
+
+            Console.WriteLine(new string('-', 20));
+            MyDelegateProgram myDelegateProgram = null;
+            MyDelegateProgram myDelegateProgram1 = new MyDelegateProgram(Method1);
+            MyDelegateProgram myDelegateProgram2 = new MyDelegateProgram(Method2);
+            MyDelegateProgram myDelegateProgram3 = new MyDelegateProgram(Method3);
+            myDelegateProgram = myDelegateProgram1 + myDelegateProgram2 + myDelegateProgram3;
+            Console.WriteLine("enter 1 to 7");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    myDelegateProgram1.Invoke();
+                    break;
+                case "2":
+                    myDelegateProgram2.Invoke();
+                    break;
+                case "3":
+                    myDelegateProgram3.Invoke();
+                    break;
+                default:
+                    break;
+
+            }
+
             // задержка
             Console.ReadKey();
         }
