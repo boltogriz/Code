91ba370777c6c2b432efcce95a010d615ff69c78
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-02-26 13:24:14 +0300

    Lesson 10, minutes 12

---
+++ C#/Essential/10_Universal_Type/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Universal_Type
+{
+     class MyClass<T>
+    {
+        public T field;
+        public void Method()
+        {
+            Console.WriteLine(field.GetType());
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> myClass = new MyClass<int>();
+            myClass.Method();
+            MyClass<string> myClass1 = new MyClass<string>();
+            myClass1.field = "filed";
+            myClass1.Method();
+            Console.ReadKey();
+        }
+    }
+}
