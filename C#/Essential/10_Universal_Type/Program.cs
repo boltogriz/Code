7ce7318b62d552c70d713dac961686dffad4f34b
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-02-27 17:30:24 +0300

    Lesson 10, minutes 30

--- C#/Essential/10_Universal_Type/Program.cs
+++ C#/Essential/10_Universal_Type/Program.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace _10_Universal_Type
 {
+    delegate R MyDelegate<T, R>(T t);
      class MyClass<T>
     {
         public T field;
         public void Method()
         {
             Console.WriteLine(field.GetType());
+        }
+        public T2 Method2<T2>(T2 field)
+        {
+            T2 filed1 = field;
+            Console.WriteLine(filed1.GetType() + " Method2<T2>");
+            return filed1;
         }
     }
     internal class Program
@@ -23,6 +30,11 @@ namespace _10_Universal_Type
             MyClass<string> myClass1 = new MyClass<string>();
             myClass1.field = "filed";
             myClass1.Method();
+            myClass.Method2<int>(32);
+            MyClass<int> myClass2 = myClass;
+            MyDelegate<int, int> myDelegate = new MyDelegate<int, int>(myClass2.Method2<int>);
+            int i = myDelegate(33);
+            Console.WriteLine(i);
             Console.ReadKey();
         }
     }
