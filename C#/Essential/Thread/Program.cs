71e6596f65971948ac85ecfd50127f940da8693b
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-03-19 12:02:09 +0300

    Lesson 13, minutes 1:00

---
+++ C#/Essential/Thread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void WriteSecond()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine("Thread ID {0}, counter = {1}", Thread.CurrentThread.GetHashCode(), counter);
+                counter++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteSecond);
+            thread.Start();
+            WriteSecond();
+            Console.ReadLine();
+        }
+    }
+}
