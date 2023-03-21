7f6ad73bea6b5032fc14160d93dd251439729e6a
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-03-21 12:25:36 +0300

    Lesson 13, minutes 1:52

---
+++ C#/Essential/Thread3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread3
+{
+    internal class Program
+    {
+        static void Method()
+        {
+            while (true)
+            {
+                Console.WriteLine("Secondary");
+                Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Method);
+            Thread thread = new Thread(threadStart);
+            thread.IsBackground = true;
+            thread.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Primery");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ C#/Essential/Thread4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread4
+{
+    class MyClass
+    {
+        object block = new object();
+        public void Method()
+        {
+            int hash = Thread.CurrentThread.GetHashCode();
+            lock (block)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Поток # {0}: шаг {1}", hash, i);
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine(new String('-', 20));
+            }
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            for(int i = 0; i < 3; i++)
+            {
+
+                new Thread(instance.Method).Start();
+                //instance.Method();
+            }
+            Thread.Sleep(500);
+            Console.ReadKey();
+        }
+    }
+}
