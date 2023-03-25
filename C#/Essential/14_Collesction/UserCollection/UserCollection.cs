193b732c59b62e8f16182d022f09308b7f16adfd
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-03-25 14:48:36 +0300

    Lesson 14, minutes 1:21

--- C#/Essential/14_Collesction/UserCollection/UserCollection.cs
+++ C#/Essential/14_Collesction/UserCollection/UserCollection.cs
@@ -5,8 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _14_Collesction
+namespace UserCollectionProject
 {
+    public class Element
+    {
+        private string name;
+        private int field1;
+        private int field2;
+        public Element(string s, int a, int b)
+        {
+            name = s;
+            field1 = a;
+            field2 = b;
+        }
+        public int Field1
+        {
+            get { return field1; }
+            set { field1 = value; }
+        }
+        public int Field2
+        {
+            get { return field2; }
+            set { field2 = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
     public class UserCollection : IEnumerable, IEnumerator
     {
          Element[] elementsArray = null;
@@ -17,6 +44,10 @@ namespace _14_Collesction
             elementsArray[1] = new Element("B", 2, 20);
             elementsArray[2] = new Element("C", 3, 30);
             elementsArray[3] = new Element("D", 4, 40);
+        }
+        public void Power()
+        {
+            Console.WriteLine("Power");
         }
         int position = -1;
         public bool MoveNext()
---
+++ C#/Essential/UserCollectionProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCollectionProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable enumerl = UserCollection.Power();
+            foreach (string element in enumerl)
+            {
+                Console.WriteLine(element);
+            }
+            //IEnumerable enumerable = UserCollection.Power();
+            //IEnumerator enumerator = enumerable.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    String element = enumerator.Current as String;
+            //    Console.WriteLine(element);
+            //}
+            Console.ReadKey();
+        }
+    }
+}
