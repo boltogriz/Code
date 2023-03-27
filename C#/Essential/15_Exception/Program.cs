f8f685151c6f4aff3b8147dafa57f95d93a2dfca
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-03-27 21:23:42 +0300

    Lesson 15, minutes 17

---
+++ C#/Essential/15_Exception/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, n = 2;
+            try
+            {
+                a = a / (2 - n);
+                Console.WriteLine(a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Оброботка исключения.");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
