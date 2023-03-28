30fbf9bcd12b55957fc91b5f1d3b5082c8cc6993
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-03-28 21:30:44 +0300

    Lesson 15, minutes 1:00

--- C#/Essential/15_Exception/Program.cs
+++ C#/Essential/15_Exception/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _15_Exception
 {
+    public class ClassWithException
+    {
+        public void ThrowInner()
+        {
+            throw new Exception("Это внутреннее исключение!");
+        }
+        public void CatchInner()
+        {
+            try
+            {
+                this.ThrowInner();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Это внешнее исключение!", e);
+            }
+        }
+    }
+    class UserException : Exception
+    {
+        public void Method()
+        {
+            Console.WriteLine("Моё исключение");
+        }
+    }
     class MyClass
     {
         public void MyMethod()
@@ -23,6 +49,44 @@ namespace _15_Exception
     {
         static void Main(string[] args)
         {
+            ClassWithException instance2 = new ClassWithException();
+            //instance2.CatchInner();
+            try 
+            {
+                instance2.CatchInner();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException.Message);
+            }
+            Console.WriteLine(new string('-',20));
+            try
+            {
+                //throw new UserException();
+                UserException usE = new UserException();
+                throw usE;
+            }
+            catch(UserException e)
+            {
+                e.Method();
+                try 
+                { 
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    FileStream fs = File.Open(@"C:\NonExistenFile.log", FileMode.Open);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                finally
+                {
+                    Console.WriteLine("finally");
+                }
+            }
             //int a = 1, n = 2;
             try
             {
