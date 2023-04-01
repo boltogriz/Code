6a91b8093b9419d910270962a0a58991c5d591c2
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-04-01 13:52:39 +0300

    Lesson 16 end

---
+++ C#/Essential/Explicit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit
+{
+    struct Digit
+    {
+        public byte value;
+        Digit(byte value)
+        {
+            this.value = value;
+        }
+        //public static explicit operator Digit(byte argument)
+        //{
+        //    Digit digit = new Digit(argument);
+        //    return digit;
+        //}
+        public static implicit operator Digit(byte argument)
+        {
+            Digit digit = new Digit(argument);
+            return digit;
+        }
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte variable = 1;
+            //Digit digit = (Digit)variable;
+            Digit digit = variable;
+            Console.WriteLine(digit);
+            Console.ReadKey();
+        }
+    }
+}
