671fe13a7212ab7d552f998d9bc75fa5bb11e5af
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-04-03 13:17:11 +0300

    Lesson 17, minutes 27

--- C#/Essential/17_Anonim/Program.cs
+++ C#/Essential/17_Anonim/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace _17_Anonim
 {
+    delegate void MyDelegate(string @string);
     internal class Program
     {
         static void Main(string[] args)
         {
+            var instMy = new
+            {
+                MyDel = new MyDelegate((string @string) => Console.WriteLine(@string))
+            };
+            instMy.MyDel("Hello");
             var instance = new { Name = "Alex", Age = 27, Id = new { Number = 123} };
             Console.WriteLine("Name = {0}, Age = {1}, ID = {2}", instance.Name, instance.Age, instance.Id.Number);
             Type type = instance.GetType();
---
+++ C#/Essential/LINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Date { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    FirstName = "first",
+                    LastName = "Ivanov",
+                    Salary = 10000,
+                    Date = DateTime.Parse("1/4/1999")
+                },
+                new Employee
+                {
+                    FirstName = "first",
+                    LastName = "Petrov",
+                    Salary = 10000,
+                    Date = DateTime.Parse("1/4/1999")
+                },
+                new Employee
+                {
+                    FirstName = "first",
+                    LastName = "Sidorov",
+                    Salary = 10000,
+                    Date = DateTime.Parse("1/4/1999")
+                },
+            };
+            for(int i = 0; i < employees.Count; i++) 
+            { 
+                Console.Write(employees[i].FirstName + " ");
+                Console.WriteLine(employees[i].LastName);
+            }
+            Console.WriteLine(employees.Last().LastName);
+            Console.ReadKey();
+        }
+    }
+}
