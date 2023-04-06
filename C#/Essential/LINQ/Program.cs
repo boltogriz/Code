a2553547d782b8fe766e8fe1e61e29c2a97d718f
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-04-06 20:43:38 +0300

    Lesson 17, minutes 49

--- C#/Essential/LINQ/Program.cs
+++ C#/Essential/LINQ/Program.cs
@@ -80,6 +80,7 @@ namespace LINQ
             int[] numbers = { 4, 7, 10 };
             int product = numbers.Aggregate(1, (int interim, int next) => interim * next);
             Console.WriteLine(product);   // output: 280
+
             Console.ReadKey();
         }
     }
