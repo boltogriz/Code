bfd74db0287c31fe5115f276b6af13d5b4658592
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-04-08 13:13:08 +0300

    Lesson 17, minutes 1:27

---
+++ C#/Essential/Dynamic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic variable = 1;
+            Console.WriteLine(variable);
+            variable = "Hello world";
+            Console.WriteLine(variable);
+            variable = DateTime.Now;
+            Console.WriteLine(variable);
+            Console.ReadLine();
+        }
+    }
+}
