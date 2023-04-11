f7de861736f45e554e5b520c53d6991950833ae3
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-04-11 14:47:21 +0300

    Lesson 18

---
+++ C#/Essential/18_SpaceName/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_SpaceName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world");
+            System.Console.ReadKey();
+        }
+    }
+}
