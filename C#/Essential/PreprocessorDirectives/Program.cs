0a8ccba87c86a450672248368f546d34aa0cae9b
Anton Kadantsev <kadantsev.anton@yandex.ru>
2023-04-15 15:43:48 +0300

    Lesson 18 end

---
+++ C#/Essential/PreprocessorDirectives/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreprocessorDirectives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            Console.WriteLine("hello");
+            #endregion
+#if DEBUG
+            Console.WriteLine("hello");
+#endif
+            Console.Read();
+        }
+    }
+}
