5f7703036db1492a239468ea5e5d7a4768cbdf14
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-07 22:56:47 +0300

    in

---
+++ C#/Essential/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int readonlyArgument = 44;
+            InArgExample(readonlyArgument);
+            Console.WriteLine(readonlyArgument);     // value is still 44
+
+            void InArgExample(in int number)
+            {
+                // Uncomment the following line to see error CS8331
+                //number = 19;
+            }
+            Console.Read();
+        }
+    }
+}
