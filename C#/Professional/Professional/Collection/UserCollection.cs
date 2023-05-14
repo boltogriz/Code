a123651bc97ba04bc5402a217a892878cde54ad9
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-14 14:02:04 +0300

    Lesson 1, minutes 20

---
+++ C#/Professional/Professional/Collection/UserCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace P_00_Collection
+{
+    internal class UserCollection : IEnumerable, IEnumerator
+    {
+        readonly Element[] elements = new Element[4];
+        public Element this[int index]
+        {
+            get { return elements[index]; }
+            set { elements[index] = value; }
+        }
+        int position = -1;
+
+        // Реализация интерфейса IEnumerator и IEnumerable
+        object IEnumerator.Current
+        { 
+            get { return elements[position]; } 
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (position < elements.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            ((IEnumerator)this).Reset();
+            return false;
+        }
+
+        void IEnumerator.Reset()
+        {
+            position = -1;
+        }
+    }
+}
--- C#/Professional/Professional/Program.cs
+++ C#/Professional/Professional/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
-namespace Professional
+namespace P_00_Collection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Element element0 = new Element(0, 0);
+            Element element1 = new Element(0, 1);
+            Element element2 = new Element(1, 0);
+            Element element3 = new Element(1, 1);
+
+            UserCollection collection = new UserCollection();
+            collection[0] = element0;
+            collection[1] = element1;
+            collection[2] = element2;
+            collection[3] = element3;
+
+            foreach(Element element in collection) 
+            {
+                Console.WriteLine("FieldA = {0}, FieldB = {1}", element.FieldA, element.FieldB);
+            }
+
+            Console.WriteLine(new string('-',20));
+
+            IEnumerator enumerator = (collection as IEnumerable).GetEnumerator();
+            while(enumerator.MoveNext()) 
+            { 
+                Element element = enumerator.Current as Element;
+                Console.WriteLine("FieldA = {0}, FieldB = {1}", element.FieldA, element.FieldB);
+            }
+
             Console.ReadKey();
         }
     }
