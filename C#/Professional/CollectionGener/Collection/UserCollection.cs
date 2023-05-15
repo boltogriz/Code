4e62818e073790e8c73cadbd71d30f5246834277
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-15 16:38:08 +0300

    Lesson 1, minutes 45

---
+++ C#/Professional/CollectionGener/Collection/UserCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CollectionGener
+{
+    internal class UserCollection<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        readonly T[] elements = new T[4];
+        public T this[int index]
+        {
+            get { return elements[index]; }
+            set { elements[index] = value; }
+        }
+        int position = -1;
+
+        //Реализация интерфейсов
+        T IEnumerator<T>.Current
+        {
+            get { return elements[position]; }
+        }
+        object IEnumerator.Current
+        { 
+            get { return elements[position]; }
+        }
+
+        void IDisposable.Dispose()
+        {
+            ((IEnumerator)this).Reset();
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (position < elements.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            ((IEnumerator)this).Reset();
+            return false;
+        }
+
+        void IEnumerator.Reset()
+        {
+            position = -1;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+    }
+}
---
+++ C#/Professional/CollectionGener2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionGener2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserCollection<Element> collection = new UserCollection<Element>();
+            collection[0] = new Element(1, 1);
+            collection[1] = new Element(2, 2);
+            collection[2] = new Element(3, 3);
+            collection[3] = new Element(4, 4);
+
+            foreach (Element element in collection)
+            {
+                Console.WriteLine($"{element.FieldA} - {element.FieldB}");
+            }
+            foreach (Element element in collection)
+            {
+                Console.WriteLine($"_{element.FieldA} - {element.FieldB}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
