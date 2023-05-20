2680659c353ad0e84c3591b4ce6b9d5666cbea42
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-20 11:42:44 +0300

    Lesson 1 end

---
+++ C#/Professional/List/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCollection
+{
+    class Elements
+    {
+        public int FolderA;
+        public int FolderB;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elements element0 = new Elements() { FolderA = 1, FolderB = 1 };
+            Elements element1 = new Elements() { FolderA = 2, FolderB = 2 };
+            Elements element2 = new Elements() { FolderA = 3, FolderB = 3 };
+            Elements element3 = new Elements() { FolderA = 4, FolderB = 3 };
+            Elements element4 = new Elements() { FolderA = 5, FolderB = 3 };
+            Elements element5 = new Elements() { FolderA = 6, FolderB = 3 };
+
+            var collection = new SimpleList() { element3, element4, element5 };
+            collection.Add(element0);
+            collection.Add(element1);
+            collection.Add(element2);
+
+            foreach (Elements item in collection)
+            {
+                Console.WriteLine("{0} - {1}", item.FolderA, item.FolderB);
+            }
+            Console.WriteLine(collection.Count);
+
+            Console.WriteLine(new string('-', 20));
+
+            collection.Remove(element1);
+
+            foreach (Elements item in collection)
+            {
+                Console.WriteLine("{0} - {1}", item.FolderA, item.FolderB);
+            }
+            Console.WriteLine(collection.Count);
+
+            collection.PrintContents();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Professional/List/SimpleList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ListCollection
+{
+    internal class SimpleList : IList
+    {
+        private readonly object[] contents = new object[8];
+        private int count;
+        public SimpleList()
+        {
+            count = 0;
+        }
+        #region Ilits Members
+        // IList
+        public object this[int index] { get => contents[index]; set => contents[index] = value; }
+
+        public bool IsReadOnly => false;
+
+        public bool IsFixedSize => true;
+
+        public int Count => count;
+
+        public object SyncRoot => null;
+
+        public bool IsSynchronized => false;
+
+        public int Add(object value)
+        {
+            if (count < contents.Length)
+            {
+                contents[count] = value;
+                count++;
+                return (count - 1);
+            }
+            return -1;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public bool Contains(object value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (contents[i] == value)
+                    return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            int j = index;
+            for (int i = 0; i <= Count; i++)
+            {
+                array.SetValue(contents[i], j);
+                j++;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            //return contents.GetEnumerator();
+            for (int i = 0; i < Count; i++)
+            {
+                yield return contents[i];
+            }
+        }
+
+        public int IndexOf(object value)
+        {
+            for (int i = 0; i < Count; i++)
+                if (contents[i] == value)
+                    return i;
+            return -1;
+        }
+
+        public void Insert(int index, object value)
+        {
+            if((count +1 <= contents.Length) && (index < Count) && (index >= 0))
+            {
+                count++;
+                for (int i = Count - 1; i > Count; i--)
+                {
+                    contents[i] = contents[i - 1];
+                }
+                contents[index] = value;
+            }
+        }
+
+        public void Remove(object value)
+        {
+            RemoveAt(IndexOf(value));
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                    contents[i] = contents[i + 1];
+                count--;
+
+            }
+        }
+        #endregion region
+        public void PrintContents()
+        {
+            Console.WriteLine("Список имеет емкость {0} и в настоящее время имеет {1} элементов.", contents.Length, count);
+            Console.WriteLine("Список содержит:");
+            for(int i = 0; i < Count;i++)
+                Console.WriteLine(" {0}", contents[i]);
+            Console.WriteLine("\n" + new string('-',55));
+        }
+    }
+}
