5069ec0b88697b91f992bd6db2157b11a50c6bba
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-21 14:40:16 +0300

    Lesson 2, minutes 20

---
+++ C#/Professional/SystemCollection_0/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SystemCollection_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            string s = "hello";
+            list.Add(s);
+            list.Add("hi");
+            list.Add(50);
+            list.Add(new object());
+            var anArray = new[] { "more", "or", "list" };
+            list.AddRange(anArray);
+            var anotherArray = new[] {new object(), new ArrayList()};
+            list.AddRange(anotherArray);
+            list.Insert(3, "Hey all");
+            var moreString = new[] { "good", "moreString", "addMoreString" };
+            list.InsertRange(3, moreString);
+            list.Remove("hello");
+            foreach (var item in list) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
