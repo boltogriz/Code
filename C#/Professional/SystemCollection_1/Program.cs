0fa3b177225f69193cc1f10f97650cfe3d2bf480
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-22 21:21:51 +0300

    Lesson 2, minutes 37

---
+++ C#/Professional/SystemCollection_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCollection_1
+{
+    class DescendingComparer : IComparer
+    {
+        readonly CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
+
+        public int Compare(object x, object y)
+        {
+            //int result = comparer.Compare(y, x);
+            int result = (int)y - (int)x;
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add(3);
+            list.Add(2);
+            list.Sort(new DescendingComparer());
+            //list.Sort();
+            foreach(var item in  list) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
