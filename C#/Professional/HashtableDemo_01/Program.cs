8f2c861dbbc418632ebd2caab1d927430fc959d1
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-23 21:05:10 +0300

    Lesson 2, minutes 1:30

---
+++ C#/Professional/HashtableDemo_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableDemo_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailLookup = new Hashtable();
+            emailLookup.Add("sbishp@cont.com", "Bishop, Scott");
+            emailLookup["s.bishop@gm.com"] = "Bishop, Scott";
+            emailLookup["surik@gm.com"] = "----";
+            Console.WriteLine(emailLookup["s.bishop@gm.com"]);
+            foreach (object email in emailLookup) 
+            {
+                Console.WriteLine(((DictionaryEntry)email).Key + " - " + ((DictionaryEntry)email).Value);
+            }
+            foreach (object email in emailLookup.Values)
+            {
+                Console.WriteLine(email);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
