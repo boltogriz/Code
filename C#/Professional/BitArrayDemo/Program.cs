e9f7becc9aca9e93f7b2657fd59d5407d33c3685
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-26 15:31:21 +0300

    Lesson 2, minutes 2:37

---
+++ C#/Professional/BitArrayDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArrayDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray bits = new BitArray(3);
+            bits[0] = false;
+            bits[1] = false;
+            bits[2] = true;
+            Console.WriteLine(bits.Length);
+            bits.Length = 4;
+            bits[3] = false;
+            Console.WriteLine(bits.Length);
+
+            var moreBits = new BitArray(4);
+            moreBits[0] = true;
+            moreBits[1] = true;
+            moreBits[2] = true;
+            moreBits[3] = true;
+
+            BitArray xorBits = bits.Xor(moreBits);
+
+            foreach (bool bit in xorBits) 
+            {
+                Console.WriteLine(bit);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Professional/ИшеМусещк32Вшьщ2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitVector32Dimo3
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vector = new BitVector32(0);
+            int firstBit = BitVector32.CreateMask();
+            int secondBit = BitVector32.CreateMask(firstBit);
+            int thirdBit = BitVector32.CreateMask(secondBit);
+            Console.WriteLine(firstBit + " " + secondBit + " " + thirdBit);
+
+            vector[firstBit] = true;
+            vector[secondBit] = true;
+
+            Console.WriteLine("{0} должно быть 3", vector.Data);
+            Console.WriteLine(vector.ToString());
+
+            var newVector = new BitVector32(4);
+            Console.WriteLine(newVector);
+
+            bool bit1 = newVector[firstBit];
+            bool bit2 = newVector[secondBit];
+            bool bit3 = newVector[thirdBit];
+
+            Console.WriteLine("{0} должно быть 4", newVector.Data);
+            Console.WriteLine(newVector.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
