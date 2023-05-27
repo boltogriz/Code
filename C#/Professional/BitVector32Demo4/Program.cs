abe31ba9f3f7235aba4f5b2b37ae2af49288e9b0
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-27 11:38:33 +0300

    Lesson 2 end

---
+++ C#/Professional/BitVector32Demo4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace BitVector32Demo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BitVector32.Section firstSection = BitVector32.CreateSection(10);
+            BitVector32.Section secondSection = BitVector32.CreateSection(50, firstSection);
+            BitVector32.Section thirdSection = BitVector32.CreateSection(500, secondSection);
+            BitVector32.Section fourthSection = BitVector32.CreateSection(500, thirdSection);
+            var packedBits = new BitVector32(0);
+
+            packedBits[firstSection] = 10;
+            packedBits[secondSection] = 50;
+            packedBits[thirdSection] = 500;
+            packedBits[fourthSection] = 499;
+
+            Console.WriteLine(packedBits[fourthSection]);
+            Console.WriteLine(packedBits[secondSection]);
+            Console.WriteLine(packedBits[thirdSection]);
+            Console.WriteLine(packedBits[fourthSection]);
+            Console.WriteLine(packedBits);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/Professional/ListGen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace ListGen
+{
+    class MyReverseComparer<T> : Comparer<T>
+    {
+        public override int Compare(T x, T y)
+        {
+            return y.GetHashCode() - x.GetHashCode();
+        }
+    }
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var intList = new List<int>() { 1, 2, 3 };
+            var reverseCompare = new MyReverseComparer<int>();
+
+            int number = intList[0];
+            Console.WriteLine("Original List");
+            intList.PrintList();
+            intList.Sort(new Comparison<int>((x, y) => y - x));
+            intList.PrintList();
+
+
+            string text = "sdf";
+            text.ShowText();
+            ShowText(text);
+
+
+            Console.ReadKey();
+        }
+        private static void PrintList<T>(this IEnumerable<T> intList)
+        {
+            foreach (T i in intList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(new string('-', 20));
+        }
+
+        private static void ShowText(this string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
