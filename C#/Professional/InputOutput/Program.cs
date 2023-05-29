be42f0a5afae91e33bc60f3de5f98a95bc4057fa
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-29 12:57:12 +0300

    Lesson 3, minutes 49

--- C#/Professional/InputOutput/Program.cs
+++ C#/Professional/InputOutput/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.ComponentModel.Design;
 
 namespace InputOutput
 {
@@ -11,20 +12,105 @@ namespace InputOutput
     {
         static void Main(string[] args)
         {
-            var directory = new DirectoryInfo(@"C:\Windows\assembly");
-            if (directory.Exists)
+            while (true)
             {
-                Console.WriteLine("FullName     : {0}", directory.FullName);
-                Console.WriteLine("Name         : {0}", directory.Name);
-                Console.WriteLine("Parent       : {0}", directory.Parent);
-                Console.WriteLine("CreationTime : {0}", directory.CreationTime);
-                Console.WriteLine("Attributes   : {0}", directory.Attributes);
+                string atrib = Console.ReadLine();
+                switch (atrib)
+                {
+                    case "deletefile":
+                        Console.WriteLine("введите имя файла для удаления");
+                        string nameFileD = Console.ReadLine();
+                        var fileInfoD = new FileInfo(nameFileD);
+                        fileInfoD.Delete();
+                        break;
+
+                    case "infofile":
+                        Console.WriteLine("введите имя файла");
+                        string fileNameT = Console.ReadLine();
+                        var fileInf = new FileInfo(fileNameT);
+                        FileStream stream = fileInf.Create();
+                        Console.WriteLine(fileInf.FullName);
+                        Console.WriteLine(fileInf.Attributes.ToString());
+                        stream.Close();
+                        break;
+
+                    case "":
+                        Console.WriteLine("неверное имя папки");
+                        break;
+
+                    case "creat":
+                        Console.WriteLine("создать папку");
+                        string atribCreat = Console.ReadLine();
+                        var directoryCreat = new DirectoryInfo(atribCreat);
+                        if (directoryCreat.Exists)
+                        {
+                            string sub = "SUBDIR";
+                            directoryCreat.CreateSubdirectory(sub);
+                            Console.WriteLine("создан {0}", directoryCreat.FullName);
+                        }
+                        else
+                        {
+                            Directory.CreateDirectory(atribCreat);
+                            var directoryCreatInf = new DirectoryInfo(atribCreat);
+                            Console.WriteLine("Создан {0}", atribCreat);
+                        }
+                        break;
+
+                    case "info":
+                        string[] drives = Directory.GetLogicalDrives();
+                        Console.WriteLine("Имеющиеся диски:");
+                        foreach (string drive in drives)
+                            Console.WriteLine("- {0}", drive);
+                        break;
+
+                    case "close":
+                        goto Close;
+
+                    case "delete":
+                        Console.WriteLine("введите имя папки для удаления");
+                        string del = Console.ReadLine();
+                        try
+                        {
+                            Directory.Delete(del);
+                        }
+                        catch (Exception ex) 
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    default:
+                        var directory = new DirectoryInfo(atrib);
+                        if (directory.Exists)
+                        {
+                            Console.WriteLine("FullName     : {0}", directory.FullName);
+                            Console.WriteLine("Name         : {0}", directory.Name);
+                            Console.WriteLine("Parent       : {0}", directory.Parent);
+                            Console.WriteLine("CreationTime : {0}", directory.CreationTime);
+                            Console.WriteLine("Attributes   : {0}", directory.Attributes);
+
+                            FileInfo[] files = directory.GetFiles("*.txt");
+                            Console.WriteLine("Найдено {0} *.txt файлов\n", files.Length);
+                            foreach (FileInfo file in files)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine(file.FullName);
+                                Console.WriteLine(file.Attributes);
+                                Console.WriteLine(file.CreationTime);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Директория с имене: {0} не существует", directory.FullName);
+                        }
+                        break;
+                }
+            Close:
+                if (atrib == "close")
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Директория с имене: {0} не существует", directory.FullName);
-            }
-            Console.ReadKey();
         }
     }
+    //Console.ReadKey();
 }
+
