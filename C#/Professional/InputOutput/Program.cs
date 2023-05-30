f2a6333086371d3b9f0cc44c85cfd8dc608748b8
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-05-30 17:04:32 +0300

    Lesson 3, minutes 1:07

--- C#/Professional/InputOutput/Program.cs
+++ C#/Professional/InputOutput/Program.cs
@@ -5,18 +5,143 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.ComponentModel.Design;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
 
 namespace InputOutput
 {
+    class Comands
+    {
+        public Dictionary<string, string[]> comands = new Dictionary<string, string[]>();
+        public Comands()
+        {
+            AddComands("deletefile", "удалить файл");
+            AddComands("stream", "создать Test.dat");
+        }
+        public void AddComands(string key, string value)
+        {
+            comands.Add(key, new string[] { key, value });
+        }
+        private string GetComand(string comand)
+        {
+            string str = "";
+            if (comands.ContainsKey(comand))
+            {
+                str = comands[comand][0];
+            }
+            else
+            {
+                comands.Add(comand, new string[] { comand, "нет описания для " + comand });
+                str = comands[comand][0];
+            }
+            return str;
+        }
+        public string Steam => GetComand("stream");
+        public string Deletefile => GetComand("deletefile");
+        public void Help()
+        {
+            foreach (var comand in comands)
+            {
+                Console.WriteLine("{0} - {1}", comand.Value[0], comand.Value[1]);
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Comands comands = new Comands();
+            Console.WriteLine(comands.Steam.GetType());
+            comands.Help();
+
             while (true)
             {
                 string atrib = Console.ReadLine();
+                if (comands.Steam == atrib)
+                {
+                    var stream = new FileStream("Test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    for (int i = 0; i < 256; i++)
+                    {
+                        stream.WriteByte((byte)i);
+                    }
+                    Console.WriteLine(stream.Position + "asdf");
+                    stream.Position = 0;
+                    stream.Close();
+                }
+
+
                 switch (atrib)
                 {
+                    case "writetext":
+                        {
+                            var file = new FileInfo("Text.txt");
+                            StreamWriter writer = file.CreateText();
+                            writer.WriteLine("First");
+                            writer.WriteLine("Second");
+                            writer.WriteLine(writer.NewLine);
+                            writer.WriteLine("Third");
+                            for (int i = 0; i < 10; i++)
+                            {
+                                writer.Write(i + " ");
+                            }
+                            writer.WriteLine(writer.NewLine);
+                            writer.Close();
+                            break;
+                        }
+                    case "memory":
+                        {
+                            var memory = new MemoryStream();
+                            memory.Capacity = 256;
+                            for (var i = 0; i < 256; i++)
+                            {
+                                memory.WriteByte((byte)i);
+                            }
+                            memory.Position = 0;
+                            for (var i = 0; i < 256; i++)
+                            {
+                                Console.Write(" " + memory.ReadByte());
+                            }
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine(new string('-', 20));
+                            Console.WriteLine();
+                            byte[] array = memory.ToArray();
+                            foreach (var item in array)
+                            {
+                                Console.Write(" " + item);
+                            }
+                            Console.WriteLine();
+                            Console.WriteLine(new string('-', 20));
+                            Console.WriteLine();
+                            var file = new FileStream("Dump.dat", FileMode.Create, FileAccess.ReadWrite);
+                            memory.WriteTo(file);
+                            file.Position = 0;
+                            for (var i = 0; i < 256; i++)
+                            {
+                                Console.Write(" "+file.ReadByte());
+                            }
+                            memory.Close();
+                            file.Close();
+                            break;
+                        }
+                    case "stream":
+                        {
+                            var stream = new FileStream("Test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                            for (int i = 0; i < 256; i++)
+                            {
+                                stream.WriteByte((byte)i);
+                            }
+                            Console.WriteLine(stream.Position);
+                            stream.Position = 0;
+                            for (int i = 0; i < 256; i++)
+                            {
+                                Console.Write(stream.Position + " - ");
+                                Console.WriteLine(" " + stream.ReadByte());
+                            }
+                            stream.Close();
+                            break;
+                        }
                     case "deletefile":
                         Console.WriteLine("введите имя файла для удаления");
                         string nameFileD = Console.ReadLine();
@@ -25,15 +150,16 @@ namespace InputOutput
                         break;
 
                     case "infofile":
-                        Console.WriteLine("введите имя файла");
-                        string fileNameT = Console.ReadLine();
-                        var fileInf = new FileInfo(fileNameT);
-                        FileStream stream = fileInf.Create();
-                        Console.WriteLine(fileInf.FullName);
-                        Console.WriteLine(fileInf.Attributes.ToString());
-                        stream.Close();
-                        break;
-
+                        {
+                            Console.WriteLine("введите имя файла");
+                            string fileNameT = Console.ReadLine();
+                            var fileInf = new FileInfo(fileNameT);
+                            FileStream stream = fileInf.Create();
+                            Console.WriteLine(fileInf.FullName);
+                            Console.WriteLine(fileInf.Attributes.ToString());
+                            stream.Close();
+                            break;
+                        }
                     case "":
                         Console.WriteLine("неверное имя папки");
                         break;
@@ -73,7 +199,7 @@ namespace InputOutput
                         {
                             Directory.Delete(del);
                         }
-                        catch (Exception ex) 
+                        catch (Exception ex)
                         {
                             Console.WriteLine(ex.Message);
                         }
