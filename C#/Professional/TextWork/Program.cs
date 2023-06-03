4578ae0aa3b54dcec9bd63f9e8756a8e99e2ed9d
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-03 11:36:07 +0300

    Lesson 4, minutes 14

---
+++ C#/Professional/TextWork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Hellow";
+            Console.WriteLine(s);
+            Console.WriteLine(s.GetHashCode());
+            s = s.Insert(2, "mm");
+            Console.WriteLine(s);
+            Console.WriteLine(s[2]);
+            String s2 = new string('-', 20);
+            s += s2;
+            string s4 = 5.ToString();
+            Console.WriteLine(s4 + s);
+            Console.ReadKey();
+        }
+    }
+}
