13b276f6962b5941ccdbd90ba83559c334adc8ca
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-05 18:55:16 +0300

    Pattern Switch

--- C#/Professional/Labirint/Program.cs
+++ C#/Professional/Labirint/Program.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Labirint
 {
     internal class Program
-    {
+    { 
+        static void SwitchRoom(ref int currentRoom, ref int nextRoom )
+        {
+            int prom = currentRoom;
+            currentRoom = nextRoom;
+            nextRoom = prom;
+        }
         static void Main(string[] args)
         {
             MazeGame mazeGame = new MazeGame();
             var maze = mazeGame.CreateMaze();
 
             string way;
-
+            string east = Direction.East.ToString();
             bool room1Done = false;
             bool room2Done = false;
+            int currentRoom = 1;
+            int nextRoom = 2;
 
             int numberRoom = 1;
             MapSite side;
@@ -32,43 +41,48 @@ namespace Labirint
                 switch (way)
                 {
                     case "north":
-                        maze.RoomNo(numberRoom).GetSide(Direction.North).Enter();
-                        side = maze.RoomNo(numberRoom).GetSide(Direction.North);
+                        maze.RoomNo(currentRoom).GetSide(Direction.North).Enter();
+                        side = maze.RoomNo(currentRoom).GetSide(Direction.North);
                         if (side is Door)
                         {
                             room1Done = true;
                             numberRoom = 2;
-                            Console.WriteLine("Ты вошел во вторую комнату");
+                            SwitchRoom(ref currentRoom, ref nextRoom);
+                            
+                            Console.WriteLine($"Ты вошел в {currentRoom} комнату");
                         }
                         break;
                     case "west":
-                        maze.RoomNo(numberRoom).GetSide(Direction.West).Enter();
-                        side = maze.RoomNo(numberRoom).GetSide(Direction.West);
+                        maze.RoomNo(currentRoom).GetSide(Direction.West).Enter();
+                        side = maze.RoomNo(currentRoom).GetSide(Direction.West);
                         if (side is Door)
                         {
                             room1Done = true;
                             numberRoom = 2;
-                            Console.WriteLine("Ты вошел во вторую комнату");
+                            SwitchRoom(ref currentRoom, ref nextRoom);
+                            Console.WriteLine($"Ты вошел в {currentRoom} комнату");
                         }
                         break;
                     case "east":
-                        maze.RoomNo(numberRoom).GetSide(Direction.East).Enter();
-                        side = maze.RoomNo(numberRoom).GetSide(Direction.East);
+                        maze.RoomNo(currentRoom).GetSide(Direction.East).Enter();
+                        side = maze.RoomNo(currentRoom).GetSide(Direction.East);
                         if (side is Door)
                         {
                             room1Done = true;
                             numberRoom = 2;
-                            Console.WriteLine("Ты вошел во вторую комнату");
+                            SwitchRoom(ref currentRoom, ref nextRoom);
+                            Console.WriteLine($"Ты вошел в {currentRoom} комнату");
                         }
                         break;
                     case "sourth":
-                        maze.RoomNo(numberRoom).GetSide(Direction.South).Enter();
-                        side = maze.RoomNo(numberRoom).GetSide(Direction.South);
+                        maze.RoomNo(currentRoom).GetSide(Direction.South).Enter();
+                        side = maze.RoomNo(currentRoom).GetSide(Direction.South);
                         if (side is Door)
                         {
                             room1Done = true;
                             numberRoom = 2;
-                            Console.WriteLine("Ты вошел во вторую комнату");
+                            SwitchRoom(ref currentRoom, ref nextRoom);
+                            Console.WriteLine($"Ты вошел в {currentRoom} комнату");
                         }
                         break;
                     default:
