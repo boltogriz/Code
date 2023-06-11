a452e59615271332640edc7ca3cb21fa028f7532
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-11 11:41:41 +0300

    Lesson 5, minutes 1:18

--- C#/Professional/XMLLanguage/Program.cs
+++ C#/Professional/XMLLanguage/Program.cs
@@ -40,6 +40,40 @@ namespace XMLLanguage
     {
         static void Main(string[] args)
         {
+            var xmlWriter = new XmlTextWriter("books.xml", Encoding.GetEncoding(1251));
+            xmlWriter.Formatting = Formatting.Indented;
+            xmlWriter.IndentChar = '\t';
+            xmlWriter.Indentation = 1;
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("ListOfBooks");
+            xmlWriter.WriteStartElement("Book");
+            xmlWriter.WriteStartAttribute("FontSize");
+            xmlWriter.WriteString("8");
+            xmlWriter.WriteEndAttribute();
+            xmlWriter.WriteString("Title-1");
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.Close();
+
+            string xmlData = $"<?xml version='1.0' encoding='iso-8859-1' ?>\r\n<ListOfBooks>\r\n  <book>\r\n    <Title FontSize =\"8\">Title-1</Title>\r\n    <Price>50</Price>\r\n  </book>\r\n  <book>\r\n    <Title FontSize =\"10\">Title-2</Title>\r\n    <Price>150</Price>\r\n  </book>\r\n</ListOfBooks>";
+
+            var stringReader = new StringReader(xmlData);
+            var reader = new XmlTextReader(stringReader);
+
+            while (reader.Read())
+            {
+                Console.WriteLine("sdf");
+                Console.WriteLine("{0, -15} {1,-15} {2, -15}",
+                    reader.NodeType.ToString(),
+                    reader.Name,
+                    reader.Value);
+            }
+            reader.Close();
+            stringReader.Close();
+
+            Console.WriteLine(new string('-',20));
+
             var document = new XmlDocument();
             document.Load("books.xml");
 
@@ -75,6 +109,37 @@ namespace XMLLanguage
                 Console.WriteLine(testReturn.I);
             }
 
+            Console.WriteLine(new string('-',20));
+
+            var document2 = new XmlDocument();
+            document2.Load("books.xml");
+
+            XmlNode root = document.DocumentElement;
+
+            Console.WriteLine(root.LocalName);
+
+            foreach(XmlNode books in root.ChildNodes)
+            {
+                Console.WriteLine("Found Book:");
+                foreach (XmlNode book in books.ChildNodes)
+                {
+                    Console.WriteLine(book.Name + ": " + book.InnerText);
+                }
+            }
+
+            Console.WriteLine(new string('-',20));
+            var reader2 = new XmlTextReader("books.xml");
+
+            while (reader2.Read())
+            {
+                if (reader2.NodeType == XmlNodeType.Element)
+                {
+                    if (reader2.Name.Equals("Title"))
+                    {
+                        Console.WriteLine("<{0}>", reader2.GetAttribute("FontSize"));
+                    }
+                }
+            }
 
             Console.ReadKey();
         }
---
+++ C#/Professional/XPathDocument/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace XPathDocumentMy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = new XPathDocument("books.xml");
+            XPathNavigator navigator = document.CreateNavigator();
+            XPathNodeIterator iterator1 = navigator.Select("ListOfBooks/book/Title");
+            foreach(var item in iterator1)
+                Console.WriteLine(item);
+
+            XPathExpression exception = navigator.Compile("ListOfBooks/book[2]/Price");
+            XPathNodeIterator iterator2 = navigator.Select(exception);
+            foreach(var item in iterator2)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
