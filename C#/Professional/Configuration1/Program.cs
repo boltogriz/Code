bf142a4bb2f6cd1b14f85eba5451bbc2b65942c5
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-11 19:20:45 +0300

    Lesson 5, minutes 1:47

---
+++ C#/Professional/Configuration1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string value = ConfigurationSettings.AppSettings["Foo"];
+            //Console.WriteLine(value);
+            //Console.WriteLine(new string('-', 20));
+
+            NameValueCollection appSetting = ConfigurationManager.AppSettings;
+            Console.WriteLine(appSetting["Foo"]);
+            Console.WriteLine(appSetting[0]);
+            Console.WriteLine(new string('-', 20));
+
+            for(int i = 0; i < appSetting.Count; i++) 
+            {
+                Console.WriteLine(appSetting.Keys[i] + " - " +appSetting[i]);
+            }
+
+            Console.WriteLine(new string('-', 20));
+            foreach(string item in appSetting) 
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(new string('-', 20));
+            Int32 counter = 0;
+            IEnumerator settingEnumerator = appSetting.Keys.GetEnumerator();
+            while (settingEnumerator.MoveNext())
+            {
+                Console.WriteLine("Key: {0} Value: {1}", appSetting.Keys[counter], appSetting[counter]);
+                counter++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Professional/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ListBox listBox2;
+        private Button buttonChooseColor;
+        private ColorDialog chooseColorDialog = new ColorDialog();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sfasdf");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
