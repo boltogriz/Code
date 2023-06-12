f305e2626a0f71cf8e4e853690bc7c878b9a4cdd
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-12 15:01:54 +0300

    Lesoon 5 end

---
+++ C#/Professional/ReestersMy/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReestersMy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey[] keys = new RegistryKey[] {Registry.ClassesRoot,
+                                                    Registry.CurrentUser,
+                                                    Registry.LocalMachine,
+                                                    Registry.Users,
+                                                    Registry.CurrentUser,
+                                                    Registry.PerformanceData
+                                                    };
+            foreach (var key in keys) 
+            {
+               
+                Console.WriteLine("{0} - всего элементов: {1}.", key.Name, key.SubKeyCount);
+            }
+
+            Console.WriteLine(new string('-',20));
+            RegistryKey myKey = Registry.LocalMachine;
+            RegistryKey software = myKey.OpenSubKey("Software");
+            RegistryKey microsoft = software.OpenSubKey("Microsoft");
+            Console.WriteLine("{0} - имеет {1} элементов.", microsoft.Name, microsoft.SubKeyCount);
+            software.Close();
+            microsoft.Close();
+
+            Console.WriteLine(new string('-',20));
+
+            software = myKey.OpenSubKey("NonExistName");
+
+            Console.WriteLine(new string('-',20));
+
+            myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("Software", true);
+            try
+            {
+                Console.WriteLine("Всего записей в {0}: {1}", wKey.Name, wKey.SubKeyCount);
+                RegistryKey newKey = wKey.CreateSubKey("MyKeyRegeester");
+                
+                Console.WriteLine("Внесен " + newKey.Name);
+                Console.WriteLine("Всего теперь " + wKey.SubKeyCount);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally 
+            { 
+                myKey.Close(); 
+            }
+
+            Console.WriteLine(new string('-',20));
+
+            RegistryKey myKey2 = Registry.CurrentUser;
+            RegistryKey wKey2 = myKey2.OpenSubKey("Software", true);
+            string[] keyNames2 = wKey2.GetSubKeyNames();
+            foreach (string keyName in keyNames2) 
+            { 
+                if (keyName == "MyKeyRegeester")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(new string(' ', 5) + keyName);
+                    Console.ForegroundColor= ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.WriteLine(new string(' ', 5) + keyName);
+
+                }
+            }
+            try 
+            {
+                Console.WriteLine("Всего {0}: {1}", wKey2.Name, wKey2.SubKeyCount);
+                wKey2.DeleteSubKey("MyKeyRegeester");
+                Console.WriteLine("удалена ветка MyKeyRegeester");
+                Console.WriteLine("Всего {0}: {1}", wKey2.Name, wKey2.SubKeyCount);
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Professional/RegisterBasics/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey key = Registry.CurrentUser;
+            RegistryKey subKey = key.OpenSubKey("Software", true);
+            RegistryKey subSubKey = subKey.CreateSubKey("CyberBionicSystematics");
+            subSubKey.SetValue("TheStringName", "I containt string");
+            subSubKey.SetValue("TheInt32Name", 123456);
+            subSubKey.SetValue("AnotherName", 1234, RegistryValueKind.String);
+            Console.WriteLine(subSubKey.GetValue("TheStringName"));
+            subKey.Close();
+
+            subKey = key.OpenSubKey(@"Software\CyberBionicSystematics");
+            string value1 = subSubKey.GetValue("TheStringName") as string;
+            subKey.Close();
+            Console.WriteLine(value1);
+
+            Console.ReadLine();
+        }
+    }
+}
