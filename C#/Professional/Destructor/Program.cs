b1f5d556d35aaf5361cec3615ef47afd7be94317
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-20 16:51:41 +0300

    Lesson 9, minutes 16

---
+++ C#/Professional/Destructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Destructor
+{
+    class SomeClass
+    {
+        ~ SomeClass() 
+        {
+            Console.WriteLine("SomeClass thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("SomeClass_Destructor");
+            Console.ReadKey();
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            SomeClass itamce = new SomeClass();
+            Console.ReadKey();
+
+        }
+    }
+}
