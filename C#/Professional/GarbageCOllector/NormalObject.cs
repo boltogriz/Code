9b7fc5806535d6321aaf1a3ba7256d6c192cac9e
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-21 20:14:31 +0300

    Lesson 9, minutes 1:06

---
+++ C#/Professional/GarbageCOllector/NormalObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCOllector
+{
+    class OtherObject
+    {
+        byte[] array = new byte[1024 * 50];
+    }
+    internal class NormalObject
+    {
+        byte[] array = new byte[1024];
+        public NormalObject() 
+        {
+            Console.WriteLine("Constructor {0}", this.GetHashCode());
+        }
+        ~ NormalObject() 
+        {
+            Console.WriteLine("Destructor {0}", this.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Professional/GarbageCOllector/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GarbageCOllector
+{
+    internal class Program
+    {
+        static void AuxiliaryMethod()
+        {
+            OtherObject[] objects = new OtherObject[1000];
+            for (int i = 0; i < objects.Length; i++) 
+            {
+                objects[i] = new OtherObject();
+                Thread.Sleep(5);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Система поддерживает {0} поколения.", (GC.MaxGeneration + 1));
+            Console.WriteLine(new string('-', 40));
+            NormalObject normalObject = new NormalObject();
+
+            new Thread(AuxiliaryMethod).Start();
+
+            for (int i = 0;i < 30;i++) 
+            {
+                Console.Write("Поколение "+GC.GetGeneration(normalObject));
+                Console.WriteLine(" | Размер кучи "+GC.GetTotalMemory(false) / 1024);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(new string ('-', 40));
+            Console.WriteLine(GC.CollectionCount(0));
+            Console.WriteLine(GC.CollectionCount(1));
+            Console.WriteLine(GC.CollectionCount(2));
+            Console.ReadLine();
+        }
+    }
+}
