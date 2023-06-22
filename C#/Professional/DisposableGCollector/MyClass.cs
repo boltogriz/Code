af94626d71284c529af46765ce8debbc92fea54f
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-22 11:20:37 +0300

    Lesson 9 end

---
+++ C#/Professional/DisposableGCollector/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DisposableGCollector
+{
+    internal class MyClass : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("MyClass_Dispose " + this.GetHashCode());
+            Thread.Sleep(2000);
+        }
+        public void ShowInf()
+        {
+            Console.WriteLine("Info");
+        }
+        ~MyClass() 
+        { 
+            for(int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(".");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Professional/DisposableGCollector/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableGCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(MyClass instance = new MyClass()) 
+            {
+                instance.ShowInf();
+                throw new Exception("using");
+            }
+
+            Console.WriteLine(new string('-',20));
+
+            MyClass instance2 = new MyClass();
+
+
+
+            try
+            {
+                instance2.ShowInf();
+                //throw new Exception("using");
+            }
+            finally
+            {
+                if(instance2 is IDisposable && instance2 != null)
+                    instance2.Dispose();
+            }
+            Console.ReadLine();
+        }
+    }
+}
