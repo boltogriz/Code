1fe2b1044d37676051e6b8e513be83baeddc57ac
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-25 21:41:42 +0300

    Lesson 10 end

---
+++ C#/Professional/AdHocPol/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdHocPol
+{
+    public class Class1 { public void Method() { Console.WriteLine("Class 1"); } }
+    public class Class2 { public void Method() { Console.WriteLine("Class 2"); } public void Method2() { Console.WriteLine("Class 2"); } }
+    public class Class3 { public void Method() { Console.WriteLine("Class 3"); } }
+    interface IInterface { void Method(); }
+    class MyClass1 : Class1, IInterface { }
+    class MyClass2 : Class2, IInterface { }
+    class MyClass3 : Class3, IInterface { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IInterface[] array = { new MyClass1(), new MyClass2(), new MyClass3() };
+            foreach (IInterface item in array)
+            {
+                item.Method();
+            }
+            IInterface inst1 = new MyClass1();
+            inst1.Method();
+            inst1 = new MyClass2();
+            inst1.Method();
+            inst1.Method2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Professional/AdHocPolimorfizm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdHocPolimorfizm
+{
+    public class Class1 { public void Method() { Console.WriteLine("Class 1"); } }
+    public class Class2 { public void Method() { Console.WriteLine("Class 2"); } }
+    public class Class3 { public void Method() { Console.WriteLine("Class 3"); } }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] array = { new Class1(), new Class2(), new Class3() };
+            foreach (dynamic item in array) 
+            { 
+                item.Method();
+            }
+
+            object inst1 = new Class1();
+            inst1 = new Class2();
+            Console.ReadKey();
+        }
+    }
+}
