f7c218824e974c5f45336a1573913d87ab67ee7e
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-27 16:17:19 +0300

    Lesson 11, minutes 49

---
+++ C#/Professional/ForeGround/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForeGround
+{
+    internal class Program
+    {
+        static void Procedure()
+        {
+            for (int i = 0; i < 1000; i++) 
+            { 
+                Thread.Sleep(10);
+                Console.Write(".");
+            }
+            Console.WriteLine("\nЗавершение вторичного потока.");
+        }
+        static void Main(string[] args)
+        {
+            var thread = new Thread(Procedure);
+            thread.IsBackground = false;
+            thread.Start();
+            Thread.Sleep(500);
+            Console.WriteLine("Завершение главного потока");
+            //Console.ReadKey();
+        }
+    }
+}
