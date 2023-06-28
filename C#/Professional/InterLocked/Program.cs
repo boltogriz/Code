9bf3ab86d63591e20986a31fe5d15a6004991f4c
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-28 17:14:52 +0300

    Lesson 11, minutes 1:55

---
+++ C#/Professional/InterLocked/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterLocked
+{
+    internal class Program
+    {
+        static long counter;
+        //static object block = new object();
+        static void Procedure()
+        {
+            for (int i = 0; i < 1000000; i++) 
+            {
+                Interlocked.Increment(ref counter);
+                //lock (block) 
+                //{ 
+                //    counter++;
+                //}
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ожидаемое значение счетчика = 10000000");
+            Thread[] threads = new Thread[10];
+            for (int i=0; i < threads.Length; i++) 
+            {
+                (threads[i] = new Thread(Procedure)).Start();
+            }
+            for (int i=0; i < threads.Length;i++) 
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine("Реальное значение счетчика  = {0}", counter);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Professional/InterLockedSpinLcok/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterLockedSpinLcok
+{
+    public class SpinLock
+    {
+        int block;
+        int wait;
+        public SpinLock(int wait)
+        {
+            this.wait = wait;
+        }
+        public void Enter()
+        {
+            int result = Interlocked.CompareExchange(ref block, 1, 0);
+            while (result == 1)
+            {
+                Thread.Sleep(wait);
+                result = Interlocked.CompareExchange(ref block, 1, 0);
+            }
+        }
+        public void Exit() 
+        { 
+            Interlocked.Exchange(ref block, 0);
+        }
+    }
+    public class SpinLockManager : IDisposable
+    {
+        SpinLock block;
+        public SpinLockManager(SpinLock block) 
+        {
+            this.block = block;
+            block.Enter();
+        }
+        public void Dispose()
+        {
+            block.Exit();
+        }
+    }
+
+    internal class Program
+    {
+        static Random random = new Random();
+        static SpinLock block = new SpinLock(10);
+
+        static FileStream stream = File.Open("log.txt", FileMode.Create, FileAccess.Write, FileShare.None);
+        static StreamWriter writer = new StreamWriter(stream);
+        static void Function()
+        {
+            using (new SpinLockManager(block))
+            {
+                writer.WriteLine("Поток {0} запускается", Thread.CurrentThread.GetHashCode());
+                writer.Flush();
+            }
+            int time = random.Next(10, 200);
+            Thread.Sleep(time);
+
+            using (new SpinLockManager(block))
+            {
+                writer.WriteLine("Поток [{0}] Завершается.", Thread.CurrentThread.GetHashCode());
+                writer.Flush();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[50];
+            for (int i = 0; i < threads.Length; ++i)
+            {
+                threads[i] = new Thread(Function);
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
