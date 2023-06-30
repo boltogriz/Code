a703f181d0473d9860ce5c3f088dae0acd762464
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-06-30 15:59:48 +0300

    Lesson 12, minutes 29

--- C#/Professional/Synhron/Program.cs
+++ C#/Professional/Synhron/Program.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Synhron
 {
+
+ 
     internal class Program
     {
+        static volatile bool stop;
         static void Main(string[] args)
         {
+
+            Thread threa = new Thread(Function);
+            threa.Start();
+            Thread.Sleep(2000);
+
+            stop = true;
+            Console.WriteLine("Main: ожидание завершения вторичного потока.");
+            threa.Join();
+            Console.ReadKey();
+               
+        }
+
+        private static void Function()
+        {
+            int x = 0;
+            while (!stop) 
+            {
+                x++;
+            }
+            Console.WriteLine("Function: поток остановлен при x = {0}.", x);
         }
     }
 }
