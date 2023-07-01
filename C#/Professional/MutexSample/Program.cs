291a7cc1d95ff62fad6030d7f4affcbda8de06aa
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-07-01 17:36:31 +0300

    Lesson 12, minutes 1:23

--- C#/Professional/MutexSample/Program.cs
+++ C#/Professional/MutexSample/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace MutexSample
 {
+    //
     internal class Program
     {
         static Mutex mutex = new Mutex(false, "MyMutex");
---
+++ C#/Professional/SemaphoreMy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreMy
+{
+    internal class Program
+    {
+        static Semaphore pool;
+        static void Function(object number)
+        {
+            pool.WaitOne();
+            Console.WriteLine("Поток {0} занял слот семафора.", number);
+            Thread.Sleep(2000);
+            Console.WriteLine("Поток {0} -----> освободил слот.", number);
+            pool.Release();
+        }
+        static void Main(string[] args)
+        {
+            pool = new Semaphore(5, 10, "MySemafore");
+            for(int i = 1; i <= 40; i++) 
+            { 
+                new Thread(Function).Start(i);
+                Thread.Sleep(500);
+            }
+            Console.ReadLine();
+        }
+    }
+}
