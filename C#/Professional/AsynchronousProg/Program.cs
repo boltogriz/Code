d4c9bc59bea6a88be968b6d032bd11fdd6378617
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-07-03 16:28:58 +0300

    Lesson 13, minutes 20

---
+++ C#/Professional/AsynchronousProg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousProg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первичный поток: Id {0}", Thread.CurrentThread.ManagedThreadId);
+
+            Action myDelegate = new Action(Method);
+            IAsyncResult asyncResult = myDelegate.BeginInvoke(null, null);
+
+            for (int i = 0; i < 80; i++) 
+            { 
+                Thread.Sleep(1000);
+                Console.Write(" 1 ");
+            }
+            myDelegate.EndInvoke(asyncResult);
+        }
+
+        private static void Method()
+        {
+            Console.WriteLine("Первичный поток: Id {0}", Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(" 2 ");
+            }
+        }
+    }
+}
