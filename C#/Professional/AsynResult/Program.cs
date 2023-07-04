8415f3aefdbde2961b5ed504d9ad28f144cdcc23
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-07-04 16:53:28 +0300

    Lesson 13, minutes 33

---
+++ C#/Professional/AsynResult/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynResult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> myDelegate = new Func<int, int, int>(Sum);
+            IAsyncResult asyncResult = myDelegate.BeginInvoke(1, 2, null, null);
+            //int result = myDelegate.EndInvoke(asyncResult);
+            //Console.WriteLine("Результ = " + result);
+
+            asyncResult.AsyncWaitHandle.WaitOne();
+            Console.WriteLine("Thread main");
+            int result = myDelegate.EndInvoke(asyncResult);
+            Console.WriteLine("Результ = " + result);
+            Console.ReadLine();
+        }
+
+        private static int Sum(int arg1, int arg2)
+        {
+            Console.WriteLine("d");
+            Thread.Sleep(5000);
+            return arg1 + arg2;
+        }
+    }
+}
