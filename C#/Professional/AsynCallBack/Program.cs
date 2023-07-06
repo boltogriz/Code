97cb2d06b981805dd3cfaa11d20a8c95c5b34a0c
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-07-06 20:48:44 +0300

    Lesson13, minutes 1:18

---
+++ C#/Professional/AsynCallBack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynCallBack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thred {0}", Thread.CurrentThread.Name);
+
+            var func = new Func<int, int, int>(Sum);
+            func.BeginInvoke(1, 2, CallBack, func);
+            Console.ReadKey();
+        }
+
+        private static void CallBack(IAsyncResult ar)
+        {
+            Func<int, int, int> caller = (Func<int, int, int>)ar.AsyncState;
+            Console.WriteLine("Thread CallBack {0}", caller.EndInvoke(ar));
+        }
+
+        private static int Sum(int arg1, int arg2)
+        {
+            return arg1 + arg2;
+        }
+    }
+}
---
+++ C#/Professional/BeginRead/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream stream = new FileStream("file.txt", FileMode.Open, FileAccess.Read);
+
+            byte[] buffer = new byte[stream.Length];
+
+            IAsyncResult asyncResult = stream.BeginRead(buffer, 0, buffer.Length, null, null);
+            Console.WriteLine("Reading");
+
+            stream.EndRead(asyncResult);
+
+            foreach(byte b in buffer) 
+            {
+                Console.Write(b + " ");
+            }
+            stream.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
