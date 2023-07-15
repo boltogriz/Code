9972c1706ba31b22a756f6beb94cdac9777ed533
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-07-15 15:22:52 +0300

    Lesson 15, minutes 19

---
+++ C#/Professional/AsyncAwait_0/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace AsyncAwait_0
+{
+    internal class MyClass
+    {
+        public MyClass()
+        {
+        }
+        public void Operation()
+        {
+            Console.WriteLine("ThreadID: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Begin");
+            Thread.Sleep(1000);
+            Console.WriteLine("End");
+        }
+        public async void OperationAsync()
+        {
+            Task task = new Task(Operation);
+            task.Start();
+            await task;
+        }
+    }
+}
---
+++ C#/Professional/AsyncAwait_0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            MyClass myClass = new MyClass();
+            myClass.OperationAsync();
+
+            Console.ReadKey();
+        }
+    }
+}
