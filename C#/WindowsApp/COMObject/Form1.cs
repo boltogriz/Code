d5ac9b08e0531b339f9c98819df538fd3e1060fa
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-04 12:04:31 +0300

    Модуль 05, часть 1 конец

---
+++ C#/WindowsApp/COMObject/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace COMObject
+{
+    public partial class Form1 : Form
+    {
+        private ApplicationClass app;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void excelButton_Click(object sender, EventArgs e)
+        {
+            app = new ApplicationClass();
+            app.Visible = true;
+            label1.Text = app.Cells[1,3].ToString() ;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (app != null)
+            { 
+                app.Quit();
+            }
+        }
+    }
+}
