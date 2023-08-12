06a273b245e473dd386a165f85a63dc98004ce5f
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-12 13:19:15 +0300

    Lesson 2 end

---
+++ C#/WpfApp/WpfAppInkCanvas/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppInkCanvas
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(InkCanvasEditingMode mode in Enum.GetValues(typeof(InkCanvasEditingMode))) 
+            {
+                lstEditingMode.Items.Add(mode);
+            }
+            lstEditingMode.SelectedIndex = (int)inkCanvas.EditingMode;
+        }
+
+        private void inkGester(object sender, InkCanvasGestureEventArgs e)
+        {
+            String gesture = "";
+            foreach (GestureRecognitionResult res in e.GetGestureRecognitionResults())
+            {
+                gesture += res.ApplicationGesture.ToString() + "; ";
+
+                gestureName.Text = gesture;
+            }
+        }
+
+
+    }
+}
