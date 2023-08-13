91f87c672c3f59390f52013ddbe499b42a0ec1b7
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-13 09:59:22 +0300

    Lesson 3, minut 22

---
+++ C#/WpfApp/WpfAppClass/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppClass
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private List<Document> documents = new List<Document>();
+        public List<Document> Documents
+        {
+            get { return documents; }
+            set { documents = value; }
+        }
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            Debug.WriteLine("----> StarUP");
+        }
+        protected override void OnActivated(EventArgs e)
+        {
+            Debug.WriteLine("----> OnActivated");
+        }
+    }
+}
