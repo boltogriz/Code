983941efe7a2413a6736dc6804b39de4b37fc1d5
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-13 20:30:05 +0300

    Lesson 4, minutes 8

---
+++ C#/WpfApp/WpfAppContent/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfAppContent
+{
+    internal class Test
+    {
+        public Test()
+        {
+        }
+        public override string ToString() 
+        {
+            return "Hello world";
+        }
+    }
+}
