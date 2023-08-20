568ee39c918affc54a320d8264dec390f07c2819
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-20 17:16:44 +0300

    Lesoon 8, minutes 7

---
+++ C#/WpfApp/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommandBinding binding = new CommandBinding(ApplicationCommands.New);
+            binding.Executed += new ExecutedRoutedEventHandler(binding_Executed);
+            this.CommandBindings.Add(binding);
+        }
+
+        private void binding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New");
+        }        
+        private void CommandBinding_Executed_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Open");
+        }
+    }
+}
