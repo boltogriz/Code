2e91f8178e48f8c89ecdcf53b83043b0733aaea7
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-22 12:21:21 +0300

    Lesson 9, minutes 27

---
+++ C#/WpfApp/MyResource/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyResource
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void getImage_Click(object sender, RoutedEventArgs e)
+        {
+            imageContaner.Source = new BitmapImage(new Uri("Images/Dolomites Seiser Alm (1920x1080).jpg", UriKind.Relative));
+        }
+    }
+}
