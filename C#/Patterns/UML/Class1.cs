b12737471a621e4aa519f303c482ce6b335d0133
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-08-31 17:29:31 +0300

    Lesson 3 Minutes 32

---
+++ C#/Patterns/UML/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UML
+{
+    public class MyClass
+    {
+        public int MyField
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void PublicMethod(int a)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ C#/Patterns/UML/MyClassLittle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UML
+{
+    public class MyClassLittle
+    {
+        public MyClass MyClass
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
