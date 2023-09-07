ca58b61cfa01ad59437a47627c203a743f08f52a
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-09-07 21:20:16 +0300

    Lesson 10 End

---
+++ C#/Patterns/Prototype/ConcretePrototype2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype
+{
+    public class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2(int id) : base(id)
+        {
+        }
+
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype2(Id);
+        }
+    }
+}
---
+++ C#/Patterns/Prototype/Prototype.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype
+{
+    public abstract class Prototype
+    {
+        public int Id { get; private set; }
+
+        public Prototype(int id)
+        {
+            this.Id = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+}
