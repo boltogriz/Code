d8332f30814a35936674e4f45358af80b2c1d3bf
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-09-13 21:44:32 +0300

    Lesson 11 End

---
+++ C#/Patterns/PatternSingleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+    
+
+            instance1.SingletonOperation();
+
+            Console.WriteLine(instance1.GetSingletonData());
+            Console.WriteLine(instance2.GetSingletonData());
+            Console.WriteLine(instance1.GetHashCode());
+            Console.WriteLine(instance2.GetHashCode());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Patterns/PatternSingleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternSingleton
+{
+    public class Singleton
+    {
+        static Singleton instance;
+        string singletonData = string.Empty;
+        protected Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+        public void SingletonOperation() 
+        {
+            singletonData = "SingletonData";
+        }
+        public string GetSingletonData()
+        {
+            return singletonData;
+        }
+    }
+}
