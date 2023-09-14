6d07d3302eef03c2352b57f48196228ea04a1703
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-09-14 20:25:11 +0300

    Lesson 12 End

---
+++ C#/Patterns/PatternAdapter/Adapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternAdapter
+{
+    public class Adapter : Target
+    { 
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+}
---
+++ C#/Patterns/PatternAdapter/Target.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternAdapter
+{
+    public abstract class Target
+    {
+        public abstract void Request();
+    }
+}
