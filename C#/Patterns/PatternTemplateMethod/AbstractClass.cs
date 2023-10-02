f64a8ce77241052b98e960f9a72588bcb28f2c3e
Антон Каданцев <kadantsev.anton@yandex.ru>
2023-10-02 20:37:31 +0300

    Lesson 28 End

---
+++ C#/Patterns/PatternTemplateMethod/AbstractClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternTemplateMethod
+{
+    public abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+        }
+    }
+}
---
+++ C#/Patterns/PatternTemplateMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternTemplateMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass instance = new ConcreteClass();
+            instance.TemplateMethod();
+
+            Console.ReadLine();
+        }
+    }
+}
